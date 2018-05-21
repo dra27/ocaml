189472f53b2a0a9b7ebeac52d1445418c39e33b4
FooB4r <bambambambooboo@gmail.com>
2018-05-21 16:40:21 +0100

    Merged unix/close.c WIP

--- otherlibs/unix/close.c
+++ otherlibs/unix/close.c
@@ -14,8 +14,36 @@
 /**************************************************************************/
 
 #include <caml/mlvalues.h>
-#include <caml/signals.h>
 #include "unixsupport.h"
+
+#ifdef _WIN32
+#include <caml/io.h>
+
+CAMLprim value unix_close(value fd)
+{
+  if (Descr_kind_val(fd) == KIND_SOCKET) {
+    if (closesocket(Socket_val(fd)) != 0) {
+      win32_maperr(WSAGetLastError());
+      uerror("close", Nothing);
+    }
+  } else {
+    /* If we have an fd then closing it also closes
+     * the underlying handle. Also, closing only
+     * the handle and not the fd leads to fd leaks. */
+    if (CRT_fd_val(fd) != NO_CRT_FD) {
+      if (_close(CRT_fd_val(fd)) != 0)
+         uerror("close", Nothing);
+    } else {
+      if (! CloseHandle(Handle_val(fd))) {
+        win32_maperr(GetLastError());
+        uerror("close", Nothing);
+      }
+    }
+  }
+  return Val_unit;
+}
+#else
+#include <caml/signals.h>
 
 CAMLprim value unix_close(value fd)
 {
@@ -26,3 +54,4 @@ CAMLprim value unix_close(value fd)
   if (ret == -1) uerror("close", Nothing);
   return Val_unit;
 }
+#endif
