9c45c3d617e066ca174a40d2aec991bbd75f01bb
David Allsopp <david.allsopp@metastack.com>
2021-01-04 17:28:27 +0000

    Control the flexdll bootstrap with configure
    On Windows and Cygwin, if the submodule is initialised, configure will
    pick it up (this can be disabled by passing --without-flexdll to
    configure). --with-flexdll can also be used to specify a different
    location for the FlexDLL sources (e.g. when building using opam) and
    sources specified this way are first copied to flexdll-sources/

--- tools/ci/appveyor/appveyor_build.sh
+++ tools/ci/appveyor/appveyor_build.sh
@@ -82,7 +82,7 @@ APPVEYOR_BUILD_FOLDER=$(echo "$APPVEYOR_BUILD_FOLDER" | cygpath -f -)
 # These directory names are specified here, because getting UTF-8 correctly
 # through appveyor.yml -> Command Script -> Bash is quite painful...
 OCAMLROOT=$(echo "$PROGRAMFILES/Бактріан🐫" | cygpath -f - -m)
-FLEXDLLROOT="$(echo "$OCAMLROOT" | cygpath -f -)/bin/flexdll"
+FLEXDLLROOT="$PROGRAMFILES/flexdll"
 
 # This must be kept in sync with appveyor_build.cmd
 BUILD_PREFIX=🐫реализация
