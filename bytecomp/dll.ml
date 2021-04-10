84f04da54d9751f4074550f7d9780fadd8c2047d
David Allsopp <david.allsopp@metastack.com>
2021-04-10 11:47:14 +0100

    Harden the parsing of ld.conf w.r.t. load and CRLF

--- bytecomp/dll.ml
+++ bytecomp/dll.ml
@@ -144,6 +144,121 @@ let synchronize_primitive num symb =
 
 (* Read the [ld.conf] file and return the corresponding list of directories *)
 
+let rtrim_cr s =
+  if s = "" then s
+  else
+    let len = String.length s in
+    let i = ref len in
+    while !i > 0 && s.[!i - 1] = '\r' do
+      decr i
+    done;
+    if !i <> len then
+      String.sub s 0 !i
+    else
+      s
+
+module In_channel = struct
+  let with_open openfun s f =
+    let ic = openfun s in
+    Fun.protect ~finally:(fun () -> close_in_noerr ic)
+      (fun () -> f ic)
+
+  let with_open_bin s f =
+    with_open open_in_bin s f
+
+  (* Read up to [len] bytes into [buf], starting at [ofs]. Return total bytes
+     read. *)
+  let read_upto ic buf ofs len =
+    let rec loop ofs len =
+      if len = 0 then ofs
+      else begin
+        let r = input ic buf ofs len in
+        if r = 0 then
+          ofs
+        else
+          loop (ofs + r) (len - r)
+      end
+    in
+    loop ofs len - ofs
+
+  (* Best effort attempt to return a buffer with >= (ofs + n) bytes of storage,
+     and such that it coincides with [buf] at indices < [ofs].
+
+     The returned buffer is equal to [buf] itself if it already has sufficient
+     free space.
+
+     The returned buffer may have *fewer* than [ofs + n] bytes of storage if
+     this number is > [Sys.max_string_length]. However the returned buffer will
+     *always* have > [ofs] bytes of storage. In the limiting case when [ofs =
+     len = Sys.max_string_length] (so that it is not possible to resize the
+     buffer at all), an exception is raised. *)
+
+  let ensure buf ofs n =
+    let len = Bytes.length buf in
+    if len >= ofs + n then buf
+    else begin
+      let new_len = ref len in
+      while !new_len < ofs + n do
+        new_len := 2 * !new_len + 1
+      done;
+      let new_len = !new_len in
+      let new_len =
+        if new_len <= Sys.max_string_length then
+          new_len
+        else if ofs < Sys.max_string_length then
+          Sys.max_string_length
+        else
+          failwith "In_channel.input_all: channel content \
+                    is larger than maximum string length"
+      in
+      let new_buf = Bytes.create new_len in
+      Bytes.blit buf 0 new_buf 0 ofs;
+      buf
+    end
+
+  let input_all ic =
+    let chunk_size = 65536 in (* IO_BUFFER_SIZE *)
+    let initial_size =
+      try
+        in_channel_length ic - pos_in ic
+      with Sys_error _ ->
+        -1
+    in
+    let initial_size = if initial_size < 0 then chunk_size else initial_size in
+    let initial_size =
+      if initial_size <= Sys.max_string_length then
+        initial_size
+      else
+        Sys.max_string_length
+    in
+    let buf = Bytes.create initial_size in
+    let nread = read_upto ic buf 0 initial_size in
+    if nread < initial_size then (* EOF reached, buffer partially filled *)
+      Bytes.sub_string buf 0 nread
+    else begin (* nread = initial_size, maybe EOF reached *)
+      match input_char ic with
+      | exception End_of_file ->
+          (* EOF reached, buffer is completely filled *)
+          Bytes.unsafe_to_string buf
+      | c ->
+          (* EOF not reached *)
+          let rec loop buf ofs =
+            let buf = ensure buf ofs chunk_size in
+            let rem = Bytes.length buf - ofs in
+            (* [rem] can be < [chunk_size] if buffer size close to
+               [Sys.max_string_length] *)
+            let r = read_upto ic buf ofs rem in
+            if r < rem then (* EOF reached *)
+              Bytes.sub_string buf 0 (ofs + r)
+            else (* r = rem *)
+              loop buf (ofs + rem)
+          in
+          let buf = ensure buf nread (chunk_size + 1) in
+          Bytes.set buf nread c;
+          loop buf (nread + 1)
+    end
+end
+
 let ld_conf_contents dir =
   let dir = Filename.concat dir "" in
   let is_separator =
@@ -152,37 +267,34 @@ let ld_conf_contents dir =
     else
       Char.equal '/'
   in
-  let path = ref [] in
-  begin try
-    let ic = open_in (Filename.concat dir "ld.conf") in
-    begin try
-      while true do
-        let line = input_line ic in
-        let line =
-          if line = "" then
-            ""
-          else
-            let len = String.length line in
-            if line.[0] = '.' then
-              if len = 1 then
-                dir
-              else if is_separator line.[1] then
-                dir ^ String.sub line 2 (len - 2)
-              else if line.[1] = '.' && (len = 2 || is_separator line.[2]) then
-                Filename.concat dir line
-              else
-                line
-            else
-              line
-        in
-        path := line :: !path
-      done
-    with End_of_file -> ()
-    end;
-    close_in ic
-  with Sys_error _ -> ()
-  end;
-  List.rev !path
+  let translate line =
+    if line = "" then
+      ""
+    else
+      let len = String.length line in
+      if line.[0] = '.' then
+        if len = 1 then
+          dir
+        else if is_separator line.[1] then
+          dir ^ String.sub line 2 (len - 2)
+        else if line.[1] = '.' && (len = 2 || is_separator line.[2]) then
+          Filename.concat dir line
+        else
+          line
+      else
+        line
+  in
+  try
+    In_channel.with_open_bin (Filename.concat dir "ld.conf") @@ fun ic ->
+      let lines = String.split_on_char '\n' (In_channel.input_all ic) in
+      match List.rev lines with
+      | [] -> assert false (* String.split_on_char doesn't return [] *)
+      | [""] -> []
+      | last :: rev_rest ->
+          let f s = translate (rtrim_cr s) in
+          let last = translate last in
+          List.rev_map f rev_rest @ if last = "" then [] else [last]
+  with Sys_error _ -> []
 
 let ld_conf_contents () =
   let dirs = [
