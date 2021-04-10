c36a25ebb4ee1ea4595f43c80f9a2682b5ddaa64
David Allsopp <david.allsopp@metastack.com>
2021-04-10 20:17:03 +0100

    Add caml_standard_library_default to the runtime
    Previously, the bytecode runtime just use OCAML_STDLIB_DIR from
    build_config.h. This value is now stored once in stdlib.o as
    caml_standard_library_default.

---
+++ runtime/stdlib.c
@@ -0,0 +1,18 @@
+/**************************************************************************/
+/*                                                                        */
+/*                                 OCaml                                  */
+/*                                                                        */
+/*                 David Allsopp, OCaml Labs, Cambridge.                  */
+/*                                                                        */
+/*   Copyright 2021 David Allsopp Ltd.                                    */
+/*                                                                        */
+/*   All rights reserved.  This file is distributed under the terms of    */
+/*   the GNU Lesser General Public License version 2.1, with the          */
+/*   special exception on linking described in the file LICENSE.          */
+/*                                                                        */
+/**************************************************************************/
+
+#include "caml/misc.h"
+#include "build_config.h"
+
+char_os *caml_standard_library_default = OCAML_STDLIB_DIR;
