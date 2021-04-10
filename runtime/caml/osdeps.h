d80e89ecf99df065b5fb6ee3329499e9669711e3
David Allsopp <david.allsopp@metastack.com>
2021-04-10 15:13:19 +0100

    Allow libdir to be found relative to bindir
    When configured with --enable-relative, the runtime uses the directory
    of the executable to determine the location of the Standard Library.
    Thus, ocamlrun and the compilers look for ../lib/ocaml by default.
    
    This is implemented by changing caml_standard_library_default to be a
    relative path, and then computing the actual value at startup (for
    bytecode) and when queried (for native, since it is only ever used if
    Dynlink is being used).
    
    Executables (and objects) produced by the compiler always have an
    absolute value of caml_standard_library_default. ocamlc.opt and
    ocamlopt.opt are built using -set-global-string to force
    caml_standard_library_default to stay a relative value.

--- runtime/caml/osdeps.h
+++ runtime/caml/osdeps.h
@@ -161,6 +161,18 @@ extern int64_t caml_time_counter(void);
 
 extern void caml_init_os_params(void);
 
+/* True if:
+   - dir equals "."
+   - dir equals ".."
+   - dir begins "./"
+   - dir begins "../"
+   The tests for null avoid the need to call strlen_os. */
+#define Is_relative_dir(dir) \
+  (dir[0] == '.' \
+   && (dir[1] == 0 \
+       || Is_dir_separator(dir[1]) \
+       || (dir[1] == '.' && (dir[2] == 0 || Is_dir_separator(dir[2])))))
+
 #endif /* CAML_INTERNALS */
 
 #ifdef _WIN32
