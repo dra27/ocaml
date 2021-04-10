83edaa9bad2047064b3ac43701aabb978addcb71
David Allsopp <david.allsopp@metastack.com>
2021-04-10 11:38:45 +0100

    Add caml_stat_memdup_to_os
    Duplicates a buffer, converting the result to char_os (i.e.
    caml_stat_strdup_to_os but allowing embedded NULLs).

--- runtime/caml/misc.h
+++ runtime/caml/misc.h
@@ -299,6 +299,7 @@ extern double caml_log1p(double);
 #define caml_stat_strdup_os caml_stat_wcsdup
 #define caml_stat_strconcat_os caml_stat_wcsconcat
 
+#define caml_stat_memdup_to_os caml_stat_memdup_to_utf16
 #define caml_stat_strdup_to_os caml_stat_strdup_to_utf16
 #define caml_stat_strdup_of_os caml_stat_strdup_of_utf16
 #define caml_copy_string_of_os caml_copy_string_of_utf16
@@ -337,6 +338,7 @@ extern double caml_log1p(double);
 #define caml_stat_strdup_os caml_stat_strdup
 #define caml_stat_strconcat_os caml_stat_strconcat
 
+#define caml_stat_memdup_to_os caml_stat_memdup
 #define caml_stat_strdup_to_os caml_stat_strdup
 #define caml_stat_strdup_of_os caml_stat_strdup
 #define caml_copy_string_of_os caml_copy_string
