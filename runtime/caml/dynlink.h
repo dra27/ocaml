c0656219a43b089789e4ab569e8a590ec7a38faa
David Allsopp <david.allsopp@metastack.com>
2021-04-10 15:02:13 +0100

    strdup entries in caml_shared_libs_path

--- runtime/caml/dynlink.h
+++ runtime/caml/dynlink.h
@@ -45,7 +45,7 @@ extern void caml_free_shared_libs(void);
 extern char_os * caml_get_stdlib_location(void);
 
 /* Parse ld.conf and add the lines read to caml_shared_libs_path */
-extern char_os * caml_parse_ld_conf(void);
+extern void caml_parse_ld_conf(void);
 
 #endif /* CAML_INTERNALS */
 
