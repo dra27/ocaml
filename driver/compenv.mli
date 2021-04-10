b3971421c81240f7b4ae4e0a60345d509273e7a6
David Allsopp <david.allsopp@metastack.com>
2021-04-10 20:17:03 +0100

    Add -set-runtime-default
    Allows the default location used by the bytecode runtime for the
    Standard Library to be overridden when creating bytecode executables.

--- driver/compenv.mli
+++ driver/compenv.mli
@@ -23,6 +23,7 @@ val print_version_and_library : string -> 'a
 val print_version_string : unit -> 'a
 val print_standard_library : unit -> 'a
 val fatal : string -> 'a
+val fatalf : ('a, unit, string, 'b) format4 -> 'a
 
 val first_ccopts : string list ref
 val first_ppx : string list ref
@@ -77,3 +78,6 @@ val process_deferred_actions :
   string * (* ocaml module extension *)
   string -> (* ocaml library extension *)
   unit
+
+(** Validate a single -set-runtime-default parameter specification. *)
+val parse_runtime_parameter : string -> unit
