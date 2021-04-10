b4ccedd6729e4e5b6055a1a3fa743a1e7a6bbf14
David Allsopp <david.allsopp@metastack.com>
2021-04-10 11:47:14 +0100

    Harden the parsing of ld.conf w.r.t. load and CRLF

--- utils/misc.mli
+++ utils/misc.mli
@@ -171,6 +171,8 @@ module Stdlib : sig
     val print : Format.formatter -> t -> unit
 
     val for_all : (char -> bool) -> t -> bool
+
+    val rtrim_cr : string -> string
   end
 
   external compare : 'a -> 'a -> int = "%compare"
