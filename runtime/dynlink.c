8c1df345638d093c36f9462cfab1df44d5323c73
David Allsopp <david.allsopp@metastack.com>
2021-04-10 20:14:25 +0100

    Lazily load ld.conf
    Custom runtime bytecode executables do not have a DLLS section: trigger
    the search for ld.conf only if there are dlls which need to be loaded.

--- runtime/dynlink.c
+++ runtime/dynlink.c
@@ -244,12 +244,13 @@ void caml_build_primitive_table(char_os * lib_path,
   if (lib_path != NULL)
     for (p = lib_path; *p != 0; p += strlen_os(p) + 1)
       caml_ext_table_add(&caml_shared_libs_path, caml_stat_strdup_os(p));
-  caml_parse_ld_conf();
   /* Open the shared libraries */
   caml_ext_table_init(&shared_libs, 8);
-  if (libs != NULL)
+  if (libs != NULL) {
+    caml_parse_ld_conf();
     for (p = libs; *p != 0; p += strlen_os(p) + 1)
       open_shared_lib(p);
+  }
   /* Build the primitive table */
   caml_ext_table_init(&caml_prim_table, 0x180);
 #ifdef DEBUG
