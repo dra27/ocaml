9edbc64d53f7c839d62b422dafc02affc5787c9f
David Allsopp <david.allsopp@metastack.com>
2021-04-10 20:17:03 +0100

    Allow libdir to be found relative to bindir
    When configured with --with-relative-libdir, the runtime uses the
    directory of the executable to determine the location of the Standard
    Library. Thus, ocamlrun and the compilers look for ../lib/ocaml by
    default.
    
    This is implemented by changing caml_standard_library_default to be a
    relative path, and then computing the actual value at startup (for
    bytecode) and when queried (for native).
    
    Executables (and objects) produced by the compiler always have an
    absolute value of caml_standard_library_default. ocamlc.opt and
    ocamlopt.opt are built using -set-runtime-default to force
    caml_standard_library_default to be a relative value.

--- runtime/win32.c
+++ runtime/win32.c
@@ -37,6 +37,7 @@
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
+#include <wchar.h>
 #include "caml/alloc.h"
 #include "caml/address_class.h"
 #include "caml/fail.h"
@@ -1036,4 +1037,90 @@ CAMLexport int caml_win32_isatty(int fd)
 int caml_num_rows_fd(int fd)
 {
   return -1;
+}
+
+CAMLextern char_os* caml_locate_standard_library (const wchar_t *exe_name,
+                                                  const wchar_t *stdlib_default,
+                                                  wchar_t **dirname)
+{
+  if (Is_relative_dir(stdlib_default)) {
+    LPWSTR root = NULL, basename, candidate, resolved_candidate = NULL;
+    DWORD l = MAX_PATH + 1, buf_len;
+    HANDLE h;
+
+    do {
+      buf_len = l;
+      caml_stat_free(root);
+      root = caml_stat_alloc(buf_len * sizeof(WCHAR));
+      l = GetFullPathName(exe_name, buf_len, root, &basename);
+    } while (l >= buf_len);
+    /* It should be an Impossible Thing for exe_name (which will have been the
+       result of GetModuleFileName) to be unparsable by GetFullPathName */
+    if (l == 0) {
+      caml_stat_free(root);
+      return caml_stat_wcsdup(stdlib_default);
+    }
+
+    CAMLassert(basename != root && Is_separator(*(basename - 1)));
+
+    /* Make root the dirname portion */
+    *(basename - 1) = 0;
+
+    candidate =
+      caml_stat_wcsconcat(3, root, CAML_DIR_SEP, stdlib_default);
+    h =
+      CreateFile(candidate, 0,
+                 FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL,
+                 OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
+    if (h == INVALID_HANDLE_VALUE) {
+      caml_stat_free(candidate);
+      return caml_stat_wcsdup(stdlib_default);
+    }
+
+    resolved_candidate = NULL;
+    l = MAX_PATH + 1;
+    do {
+      buf_len = l;
+      caml_stat_free(resolved_candidate);
+      resolved_candidate = caml_stat_alloc(buf_len * sizeof(WCHAR));
+      l = GetFinalPathNameByHandle(h, resolved_candidate, buf_len,
+                                   FILE_NAME_NORMALIZED | VOLUME_NAME_DOS);
+    } while (l >= buf_len);
+
+    if (l > 0) {
+      /* GetFinalPathNameByHandle always returns \\?\ which needs stripping.
+         UNC paths are returned as \\?\UNC\ - in this case, reuse the \\ from
+         \\?\. */
+      CAMLassert(l > 4 && resolved_candidate[0] == '\\'
+                       && resolved_candidate[1] == '\\'
+                       && resolved_candidate[2] == '?'
+                       && resolved_candidate[3] == '\\');
+
+      caml_stat_free(candidate);
+
+      if (l >= 8 && resolved_candidate[4] == 'U'
+                 && resolved_candidate[5] == 'N'
+                 && resolved_candidate[6] == 'C'
+                 && resolved_candidate[7] == '\\') {
+        /* NT native UNC path - reuse the 'C' for the backslash */
+        resolved_candidate[6] = '\\';
+        candidate = caml_stat_wcsdup(resolved_candidate + 6);
+      } else {
+        /* Local device path */
+        candidate = caml_stat_wcsdup(resolved_candidate + 4);
+      }
+    }
+
+    /* It should be another Impossible Thing for l == 0 in the above. If that
+       did happen, candidate will _not_ have been freed, and we'll return the
+       path returned by GetFullPathName */
+    caml_stat_free(resolved_candidate);
+
+    if (dirname != NULL)
+      *dirname = caml_stat_wcsdup(root);
+    caml_stat_free(root);
+    return candidate;
+  } else {
+    return caml_stat_wcsdup(stdlib_default);
+  }
 }
