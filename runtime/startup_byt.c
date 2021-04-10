54ea04fa82a0ad78274f472708b176f55a7a091f
David Allsopp <david.allsopp@metastack.com>
2021-04-10 15:13:19 +0100

    Allow libdir to be found relative to bindir
    When configured with --with-relative-libdir, the runtime uses the
    directory of the executable to determine the location of the Standard
    Library. Thus, ocamlrun and the compilers look for ../lib/ocaml by
    default.
    
    This is implemented by changing caml_standard_library_default to be a
    relative path, and then computing the actual value at startup (for
    bytecode) and when queried (for native, since it is only ever used if
    Dynlink is being used).
    
    Executables (and objects) produced by the compiler always have an
    absolute value of caml_standard_library_default. ocamlc.opt and
    ocamlopt.opt are built using -set-global-string to force
    caml_standard_library_default to stay a relative value.

--- runtime/startup_byt.c
+++ runtime/startup_byt.c
@@ -72,6 +72,8 @@
 #define SEEK_END 2
 #endif
 
+const char_os * caml_standard_library_effective = NULL;
+
 static char magicstr[EXEC_MAGIC_LENGTH+1];
 
 /* Print the specified error message followed by an end-of-line and exit */
@@ -466,7 +468,7 @@ CAMLexport void caml_main(char_os **argv)
   value res;
   char * req_prims;
   char_os * shared_lib_path, * shared_libs;
-  char_os * exe_name, * proc_self_exe;
+  char_os * exe_name, * proc_self_exe, * argv0;
 
   /* Determine options */
   caml_parse_ocamlrunparam();
@@ -487,7 +489,7 @@ CAMLexport void caml_main(char_os **argv)
   /* Determine position of bytecode file */
   pos = 0;
 
-  proc_self_exe = caml_executable_name();
+  argv0 = proc_self_exe = caml_executable_name();
 
   if (caml_byte_program_mode != APPENDED || proc_self_exe == NULL) {
     /* First, try argv[0] (when ocamlrun is called by a bytecode program) */
@@ -510,9 +512,16 @@ CAMLexport void caml_main(char_os **argv)
       error("unable to open file '%s'", caml_stat_strdup_of_os(exe_name));
   }
 
+  if (argv0 == NULL)
+    argv0 = caml_search_exe_in_path(exe_name);
+
   if (fd < 0) {
     pos = parse_command_line(argv);
     if (caml_params->print_config) {
+      caml_standard_library_effective =
+        caml_locate_standard_library(argv0,
+                                     caml_standard_library_default, NULL);
+
       do_print_config();
       exit(0);
     }
@@ -544,6 +553,20 @@ CAMLexport void caml_main(char_os **argv)
   /* Read the table of contents (section descriptors) */
   caml_read_section_descriptors(fd, &trail);
 
+  caml_standard_library_effective =
+    caml_locate_standard_library(argv0, caml_standard_library_default, NULL);
+
+  /* Load the embedded overridden caml_standard_library_default value, if one is
+     available. This value is set _after_ caml_standard_library_effective has
+     been called, because ocamlrun must use the value it was configured with.
+     For -custom executables, the value is the same - but they specify
+     caml_standard_library_default with the primitives object, rather than via
+     the OSLD section. */
+  char_os *image_standard_library_default =
+    read_section_to_os(fd, &trail, "OSLD");
+  if (image_standard_library_default != NULL)
+    caml_standard_library_default = image_standard_library_default;
+
   /* Initialize the abstract machine */
   caml_init_gc ();
 
@@ -567,20 +590,6 @@ CAMLexport void caml_main(char_os **argv)
   req_prims = read_section(fd, &trail, "PRIM");
   if (req_prims == NULL) caml_fatal_error("no PRIM section");
   caml_build_primitive_table(shared_lib_path, shared_libs, req_prims);
-  /* Load the embedded overridden caml_standard_library_default value, if one is
-     available. This value is set _after_ caml_build_primitive_table has been
-     called, because ocamlrun must use the value it was configured with. Note
-     that although -custom executables come through this mechanism, they don't
-     have any DLLS to load.
-     XXX Intend to change that - -custom executables should embed their
-         standard_library_default in the same way as ocamlrun, but also set the
-         appropriate flag to disable all the DLLS section machinery, meaning
-         that they won't use this mechanism at all (at present -custom
-         executables can still have OSLD sections) */
-  char_os *image_standard_library_default =
-    read_section_to_os(fd, &trail, "OSLD");
-  if (image_standard_library_default != NULL)
-    caml_standard_library_default = image_standard_library_default;
   caml_stat_free(shared_lib_path);
   caml_stat_free(shared_libs);
   caml_stat_free(req_prims);
@@ -592,7 +601,7 @@ CAMLexport void caml_main(char_os **argv)
   caml_close_channel(chan); /* this also closes fd */
   caml_stat_free(trail.section);
   /* Initialize system libraries */
-  caml_sys_init(proc_self_exe, exe_name, argv + pos);
+  caml_sys_init(proc_self_exe, argv[0], exe_name, argv + pos);
   /* Load debugging info, if b>=2 */
   caml_load_main_debug_info();
   /* ensure all globals are in major heap */
@@ -659,6 +668,9 @@ CAMLexport value caml_startup_code_exn(
   else
     exe_name = proc_self_exe;
 
+  caml_standard_library_effective =
+    caml_locate_standard_library(exe_name, caml_standard_library_default, NULL);
+
   Caml_state->external_raise = NULL;
   /* Setup signal handling */
   caml_init_signals();
@@ -680,7 +692,7 @@ CAMLexport value caml_startup_code_exn(
   caml_modify_generational_global_root
     (&caml_global_data, caml_input_value_from_block(data, data_size));
   /* Initialize system libraries */
-  caml_sys_init(proc_self_exe, exe_name, argv);
+  caml_sys_init(proc_self_exe, argv[0], exe_name, argv);
   /* Load debugging info, if b>=2 */
   caml_load_main_debug_info();
   /* ensure all globals are in major heap */
