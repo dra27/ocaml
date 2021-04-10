a027e500f77c78f77a3ea25e82f3c82e6c789246
David Allsopp <david.allsopp@metastack.com>
2021-04-10 20:17:03 +0100

    Allow libdir to be found relative to bindir
    When configured with --with-relative-libdir, the runtime uses the
    directory of the executable to determine the location of the Standard
    Library. Thus, ocamlrun and the compilers look for ../lib/ocaml by
    default.
    
    This is implemented by changing caml_standard_library_default to be a
    relative path, and then computing the actual value at startup (for
    bytecode) and when queried (for native).
    
    Executables (and objects) produced by the compiler always have an
    absolute value of caml_standard_library_default. ocamlc.opt and
    ocamlopt.opt are built using -set-runtime-default to force
    caml_standard_library_default to be a relative value.

--- runtime/dynlink.c
+++ runtime/dynlink.c
@@ -249,7 +249,7 @@ CAMLprim value caml_dynlink_parse_runtime_ld_conf(value ignored)
      is built, but this primitive is never actually called. */
   return parse_ld_conf(T(""));
 #else
-  return parse_ld_conf(caml_runtime_standard_library_default);
+  return parse_ld_conf(caml_runtime_standard_library_effective);
 #endif
 }
 
@@ -305,7 +305,7 @@ void caml_build_primitive_table(char_os * lib_path,
   if (lib_path != NULL)
     for (p = lib_path; *p != 0; p += strlen_os(p) + 1)
       caml_ext_table_add(&caml_shared_libs_path, p);
-  tofree2 = caml_parse_ld_conf(caml_runtime_standard_library_default,
+  tofree2 = caml_parse_ld_conf(caml_runtime_standard_library_effective,
                                &caml_shared_libs_path);
   /* Open the shared libraries */
   caml_ext_table_init(&shared_libs, 8);
