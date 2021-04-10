930bd4ab5fb8ec3a51fc897aab41651c0676f11a
David Allsopp <david.allsopp@metastack.com>
2021-04-10 15:03:03 +0100

    ld.conf-relative path interpretation
    Explicit relative paths in ld.conf are now interpreted relative to the
    directory containing ld.conf.

--- runtime/caml/osdeps.h
+++ runtime/caml/osdeps.h
@@ -145,6 +145,14 @@ CAMLextern value caml_win32_xdg_defaults(void);
 
 CAMLextern value caml_win32_get_temp_path(void);
 
+#define CAML_DIR_SEP T("\\")
+#define Is_dir_separator(c) (c == '\\' || c == '/')
+
+#else
+
+#define CAML_DIR_SEP "/"
+#define Is_dir_separator(c) (c == '/')
+
 #endif /* _WIN32 */
 
 /* Returns the current value of a counter that increments once per nanosecond.
