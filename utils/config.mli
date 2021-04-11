f0b753179f6419b2242c97634c3e61f9627e737b
David Allsopp <david.allsopp@metastack.com>
2021-04-11 16:12:02 +0100

    Build suffixed shared runtimes
    New names for libcamlrun_shared.so and libasmrun_shared.so without the
    _shared suffix and using the target triplet and runtime ID. Both ocamlc
    and ocamlopt explicitly recognise `-runtime-variant _shared` and select
    the correct name.
    
    Symbolic links for libcamlrun_shared.so and libasmrun_shared.so to allow
    any C programs which linked against the the output of `-output-obj` to
    continue to work.

--- utils/config.mli
+++ utils/config.mli
@@ -366,8 +366,8 @@ type search_method =
 | Search               (** Always search for the interpreter *)
 
 val suffixing : bool
-(** Whether the runtime executable filenames are being mangled with Runtime IDs
-    and the {!target}.
+(** Whether the runtime executable and shared library filenames are being
+    mangled with Runtime IDs and the {!target}.
 
     @since 5.5 *)
 
