a048bf301f04d7735b99f49a49cdbbeebffae64c
David Allsopp <david.allsopp@metastack.com>
2021-04-11 16:12:02 +0100

    Build suffixed shared runtimes
    New names for libcamlrun_shared.so and libasmrun_shared.so without the
    _shared suffix and using the target triplet and runtime ID. Both ocamlc
    and ocamlopt explicitly recognise `-runtime-variant _shared` and select
    the correct name.
    
    Symbolic links for libcamlrun_shared.so and libasmrun_shared.so to allow
    any C programs which linked against the the output of `-output-obj` to
    continue to work.

--- testsuite/tools/test_in_prefix.ml
+++ testsuite/tools/test_in_prefix.ml
@@ -1867,8 +1867,9 @@ let compile_test ~original env bindir libdir =
             ["-output-complete-obj"; "-noautolink"; "-cclib"; "-lunixnat";
                                                     "-cclib"; "-lcomprmarsh"]
       | Output_complete_obj(C_ocamlopt, Shared) ->
-          (* ocamlopt doesn't correctly implement -runtime-variant _shared *)
-          let compilation_exit_code = fails_if true in
+          (* ocamlopt allows the .so to be passed to the partial linker which
+             fails with GNU ld, but not with the macOS linker *)
+          let compilation_exit_code = fails_if (Config.system <> "macosx") in
           f ~needs_ocamlopt:true ~use_shared_runtime:true
             ~compilation_exit_code ~clibs:[Config.compression_c_libraries]
             ["-output-complete-obj"; "-noautolink"; "-cclib"; "-lunixnat";
@@ -2494,7 +2495,8 @@ let test_relocation prefix bindir libdir =
       let file =
         Filename.chop_suffix_opt ~suffix:".exe" file
         |> Option.value ~default:file in
-      let ext = Filename.extension file in
+      let basename = Filename.basename file in
+      let ext = Filename.extension basename in
       let build =
         if StringSet.mem ext libdir_exts_with_build then
           LocationSet.singleton Build
@@ -2503,7 +2505,7 @@ let test_relocation prefix bindir libdir =
           LocationSet.empty
         else if ext = Config.ext_lib then
           if clang_cl then
-            if String.starts_with ~prefix:"libasmrun" (Filename.basename file)
+            if String.starts_with ~prefix:"libasmrun" basename
                || (Sys.file_exists
                      (Filename.remove_extension file ^ ".cmxa")) then
               LocationSet.singleton Build
