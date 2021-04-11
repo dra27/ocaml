c72b2aab4745801fe26a89bdc4cfd26d44f2c942
David Allsopp <david.allsopp@metastack.com>
2021-04-11 16:12:02 +0100

    Mangle the bytecode runtime executable names
    New option --disable-suffixing controls whether the build should use any
    of the computed values for mangling its own files.

--- bytecomp/bytesections.mli
+++ bytecomp/bytesections.mli
@@ -97,7 +97,8 @@ type search_mode =
 | Search
     (** Always search for the interpreter *)
 
-val read_runtime : section_table -> in_channel -> string * search_mode
+val read_runtime :
+  section_table -> in_channel -> string * Misc.RuntimeID.t option * search_mode
 (** Returns the runtime used by this tendered/standalone image
 
     @raise Not_found For an image linked using -without-runtime *)
