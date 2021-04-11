d9347d9fa511e6dd5f1af00ece372516bb32edf8
David Allsopp <david.allsopp@metastack.com>
2021-04-11 16:12:02 +0100

    Mangle the bytecode runtime executable names
    New option --disable-suffixing controls whether the build should use any
    of the computed values for mangling its own files.

--- testsuite/tools/harness.ml
+++ testsuite/tools/harness.ml
@@ -113,6 +113,7 @@ module Import = struct
   | Tendered of {header: launch_mode;
                  dlls: bool;
                  runtime: string;
+                 id: Misc.RuntimeID.t option;
                  search: Byterntm.search_method}
   | Custom
   | Vanilla
@@ -130,7 +131,9 @@ module Import = struct
     target_launcher_searches_for_ocamlrun: bool;
     bytecode_shebangs_by_default: bool;
     shebangscripts: bool;
-    libraries: string list list
+    filename_mangling: bool;
+    libraries: string list list;
+    zinc_bootstrapped: bool
   }
 
   module Char = Char
