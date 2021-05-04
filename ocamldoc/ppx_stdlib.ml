5a0e58df5088d91bf2a453cfe0911071492eb6f0
David Allsopp <david.allsopp@metastack.com>
2021-05-04 14:37:50 +0100

    Use a small ppx for ocamldoc
    Eliminates the expand_module_aliases.awk script completely.

---
+++ ocamldoc/ppx_stdlib.ml
@@ -0,0 +1,19 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                 David Allsopp, OCaml Labs, Cambridge.                  *)
+(*                                                                        *)
+(*   Copyright 2021 David Allsopp Ltd.                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+let () =
+  Ast_mapper.register "stdlib-since" (fun _ -> Compiler_ppx.labelled_since ())
+
+let () =
+  Ast_mapper.register "stdlib-aliases" (fun _ -> Compiler_ppx.stdlib_aliases ())
