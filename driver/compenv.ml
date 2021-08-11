12710a67aa601c76679d9f29a58cf2e94e363483
David Allsopp <david.allsopp@metastack.com>
2021-08-11 17:25:09 +0100

    Add -set-runtime-default
    Allows the default location used by the bytecode runtime for the
    Standard Library to be overridden when creating bytecode executables.

--- driver/compenv.ml
+++ driver/compenv.ml
@@ -39,6 +39,8 @@ let fatal err =
   prerr_endline err;
   exit 2
 
+let fatalf fmt = Printf.ksprintf fatal fmt
+
 let extract_output = function
   | Some s -> s
   | None ->
@@ -676,4 +678,15 @@ let process_deferred_actions env =
     !print_types ||
     match !stop_after with
     | None -> false
-    | Some p -> Clflags.Compiler_pass.is_compilation_pass p;
+    | Some p -> Clflags.Compiler_pass.is_compilation_pass p
+
+let parse_runtime_parameter opt =
+  let k, setting =
+    try Misc.cut_at opt '='
+    with Not_found ->
+      fatalf "-set-runtime-default: invalid runtime parameter '%s'. \
+              Expected <name>=<value>." opt in
+    if k = "standard_library_default" then
+      Clflags.standard_library_default := Some setting
+    else
+      fatalf "-set-runtime-default: unrecognized runtime parameter %s." k
