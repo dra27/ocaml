7edd0fbdd12b79f5faf7b2a3d67f9fba1ffd4aef
David Allsopp <david.allsopp@metastack.com>
2021-08-11 17:25:09 +0100

    Add -set-runtime-default
    Allows the default location used by the bytecode runtime for the
    Standard Library to be overridden when creating bytecode executables.

--- driver/main_args.ml
+++ driver/main_args.ml
@@ -143,6 +143,10 @@ let mk_I f =
   "-I", Arg.String f, "<dir>  Add <dir> to the list of include directories"
 ;;
 
+let mk_set_runtime_default f =
+  "-set-runtime-default", Arg.String f, "<param>=<value>  Set the default for \
+      runtime parameter <param> to <value>"
+
 let mk_impl f =
   "-impl", Arg.String f, "<file>  Compile <file> as a .ml file"
 ;;
@@ -966,6 +970,7 @@ module type Compiler_options = sig
   val _with_runtime : unit -> unit
   val _without_runtime : unit -> unit
   val _safe_string : unit -> unit
+  val _set_runtime_default : string -> unit
   val _short_paths : unit -> unit
   val _thread : unit -> unit
   val _v : unit -> unit
@@ -1185,6 +1190,7 @@ struct
     mk_with_runtime F._with_runtime;
     mk_without_runtime F._without_runtime;
     mk_safe_string F._safe_string;
+    mk_set_runtime_default F._set_runtime_default;
     mk_short_paths F._short_paths;
     mk_strict_sequence F._strict_sequence;
     mk_no_strict_sequence F._no_strict_sequence;
@@ -1377,6 +1383,7 @@ struct
     mk_without_runtime F._without_runtime;
     mk_S F._S;
     mk_safe_string F._safe_string;
+    mk_set_runtime_default F._set_runtime_default;
     mk_shared F._shared;
     mk_short_paths F._short_paths;
     mk_strict_sequence F._strict_sequence;
@@ -1834,6 +1841,7 @@ module Default = struct
     let _plugin _p = plugin := true
     let _pp s = preprocessor := (Some s)
     let _runtime_variant s = runtime_variant := s
+    let _set_runtime_default s = Compenv.parse_runtime_parameter s
     let _stop_after pass =
       let module P = Compiler_pass in
         match P.of_string pass with
