7e348d96e2b9655822b4c1d2970e4d8ff6bc02a3
David Allsopp <david.allsopp@metastack.com>
2021-08-11 17:16:24 +0100

    Use %standard_library_default in Config
    Config.standard_library_default is now implemented using the
    %standard_library_default primitive. This allows a convenient test which
    can be added for `-set-runtime-default`.
    
    The change also makes the host-like nature of of
    Config.standard_library_default clearer, as the build of the
    cross-compiler must now (correctly) specify the location of its (target)
    Standard Library.

--- runtime/dynlink.c
+++ runtime/dynlink.c
@@ -205,18 +205,36 @@ CAMLexport char_os * caml_parse_ld_conf(const char_os * stdlib,
   return result;
 }
 
+#ifndef NATIVE_CODE
+static int stdlib_override = 0;
+#endif
+
 /* Exposes caml_parse_ld_conf as a primitive for the bytecode compiler, saving
    the duplication of the logic with the bytecode compiler. */
-static value parse_ld_conf(const char_os *stdlib)
+CAMLprim value caml_dynlink_parse_ld_conf(value vstdlib)
 {
-  CAMLparam0();
+  CAMLparam1(vstdlib);
   CAMLlocal3(list, str, cell);
 
+  char_os *stdlib;
   struct ext_table table;
   char_os *tofree;
   int i;
+#ifndef NATIVE_CODE
+  if (stdlib_override)
+    stdlib = (char_os *)caml_runtime_standard_library_default;
+  else
+#endif
+    stdlib = caml_stat_strdup_to_os(String_val(vstdlib));
   caml_ext_table_init(&table, 8);
   tofree = caml_parse_ld_conf(stdlib, &table);
+#ifndef NATIVE_CODE
+  if (!stdlib_override)
+#endif
+    caml_stat_free(stdlib);
+#ifndef NATIVE_CODE
+  stdlib_override = 0;
+#endif
 
   list = Val_emptylist;
   for (i = table.size - 1; i >= 0; i--) {
@@ -233,27 +251,13 @@ static value parse_ld_conf(const char_os *stdlib)
   CAMLreturn(list);
 }
 
-CAMLprim value caml_dynlink_parse_ld_conf(value vstdlib)
-{
-  char_os *stdlib = caml_stat_strdup_to_os(String_val(vstdlib));
-  value res = parse_ld_conf(stdlib);
-  caml_stat_free(stdlib);
-
-  return res;
-}
+#ifndef NATIVE_CODE
 
 CAMLprim value caml_dynlink_parse_runtime_ld_conf(value ignored)
 {
-#ifdef NATIVE_CODE
-  /* This primitive is needed in native code because of how dynlink_compilerlibs
-     is built, but this primitive is never actually called. */
-  return parse_ld_conf(T(""));
-#else
-  return parse_ld_conf(caml_runtime_standard_library_default);
-#endif
+  stdlib_override = 1;
+  return Val_unit;
 }
-
-#ifndef NATIVE_CODE
 
 /* Open the given shared library and add it to shared_libs.
    Abort on error. */
