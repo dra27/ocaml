87607338de5ae995c88a8f865448521d5309081c
David Allsopp <david.allsopp@metastack.com>
2021-08-11 17:25:09 +0100

    Add caml_os_bytes_of_string primitive
    Converts an OCaml string in UTF-8 to a bytes consisting of the char_os
    representation of that string.

--- runtime/str.c
+++ runtime/str.c
@@ -557,3 +557,13 @@ CAMLprim value caml_format_c_string_literal(value v)
 
   return result;
 }
+
+CAMLprim value caml_os_bytes_of_string(value v)
+{
+  size_t len = caml_string_length(v) + 1;
+  char_os *s = caml_stat_char_array_to_os(String_val(v), len, &len);
+  value result =
+    caml_alloc_initialized_string(len * sizeof(char_os), (char *)s);
+  caml_stat_free(s);
+  return result;
+}
