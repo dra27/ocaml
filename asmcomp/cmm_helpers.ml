3e695d129dac0b0da4613f102c3ec7620071af15
David Allsopp <david.allsopp@metastack.com>
2021-08-11 17:16:24 +0100

    Add %standard_library_default
    %standard_library_default allows Config.standard_library_default to be
    converted to a compile-time derived value, as with existing compile-time
    constants such as %backend_type, etc. This paves the way for allowing
    Config.standard_library_default to be changed at link-time, rather than
    fixed when the Config module itself is compiled.

--- asmcomp/cmm_helpers.ml
+++ asmcomp/cmm_helpers.ml
@@ -2678,6 +2678,9 @@ let predef_exception i name =
   in
   Cdata data_items
 
+let emit_global_string_constant name value =
+  Cdata (emit_string_constant (name, Global) value [])
+
 (* Header for a plugin *)
 
 let plugin_header units =
