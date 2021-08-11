ed4d5248ca6126239b507e61807a7f2bee916133
David Allsopp <david.allsopp@metastack.com>
2021-08-11 17:16:24 +0100

    Add caml_stat_strndup_{to,of}_os
    Allows conversion of arbitrary char/wchar_t sequences by allowing the
    number of characters copied to be specified. The output length of the
    encoding can also be returned by these functions.

--- runtime/caml/misc.h
+++ runtime/caml/misc.h
@@ -211,6 +211,8 @@ extern int caml_umul_overflow(uintnat a, uintnat b, uintnat * res);
 
 #define caml_stat_strdup_to_os caml_stat_strdup_to_utf16
 #define caml_stat_strdup_of_os caml_stat_strdup_of_utf16
+#define caml_stat_strndup_to_os caml_stat_strndup_to_utf16
+#define caml_stat_strndup_of_os caml_stat_strndup_of_utf16
 #define caml_copy_string_of_os caml_copy_string_of_utf16
 
 #else /* _WIN32 */
@@ -243,6 +245,8 @@ extern int caml_umul_overflow(uintnat a, uintnat b, uintnat * res);
 
 #define caml_stat_strdup_to_os caml_stat_strdup
 #define caml_stat_strdup_of_os caml_stat_strdup
+#define caml_stat_strndup_to_os caml_stat_strndup
+#define caml_stat_strndup_of_os caml_stat_strndup
 #define caml_copy_string_of_os caml_copy_string
 
 #endif /* _WIN32 */
