1594619c99659c29a72f39ca9e7d02b6a5ec2883
David Allsopp <david.allsopp@metastack.com>
2021-09-11 16:52:08 +0100

    Add runtime suffixes to bytecode stub libraries
    ocamlc -dllib-suffixed appends the runtime's host triplet and bytecode
    runtime ID to the supplied name when searching for the DLL, and records
    the base name only in .cma / executable files.
    
    ocamlmklib -suffixed instructs ocamlmklib to use -dllib-suffixed when
    generating .cma files instead of -dllib.
    
    The effect is that stub libraries built this way have names which will
    be unique for a given configuration of OCaml and so will be ignored by
    other runtimes.

--- ocamltest/ocamltest_config.mli
+++ ocamltest/ocamltest_config.mli
@@ -124,3 +124,7 @@ val naked_pointers : bool
 
 val has_relative_libdir : bool
 (** Whether the compiler has been configured using --with-relative-libdir *)
+
+val suffixing : bool
+(** Whether C stub library filenames are being mangled with the Bytecode
+    Runtime ID and {!Config.target}. *)
