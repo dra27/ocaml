fb0de585612e8f62b95c8ee1989b352429230f57
David Allsopp <david.allsopp@metastack.com>
2021-09-11 16:52:08 +0100

    Add runtime suffixes to bytecode stub libraries
    ocamlc -dllib-suffixed appends the runtime's host triplet and bytecode
    runtime ID to the supplied name when searching for the DLL, and records
    the base name only in .cma / executable files.
    
    ocamlmklib -suffixed instructs ocamlmklib to use -dllib-suffixed when
    generating .cma files instead of -dllib.
    
    The effect is that stub libraries built this way have names which will
    be unique for a given configuration of OCaml and so will be ignored by
    other runtimes.

--- bytecomp/bytelink.ml
+++ bytecomp/bytelink.ml
@@ -167,6 +167,15 @@ module String = struct
       else if p (unsafe_get s i) then true
       else loop (succ i) in
     loop 0
+
+  let starts_with ~prefix s =
+    let len_s = length s
+    and len_pre = length prefix in
+    let rec aux i =
+      if i = len_pre then true
+      else if unsafe_get s i <> unsafe_get prefix i then false
+      else aux (i + 1)
+    in len_s >= len_pre && aux 0
 end
 
 type error =
--- bytecomp/dll.ml
+++ bytecomp/dll.ml
@@ -15,6 +15,19 @@
 
 (* Handling of dynamically-linked libraries *)
 
+module String = struct
+  include String
+
+  let starts_with ~prefix s =
+    let len_s = length s
+    and len_pre = length prefix in
+    let rec aux i =
+      if i = len_pre then true
+      else if unsafe_get s i <> unsafe_get prefix i then false
+      else aux (i + 1)
+    in len_s >= len_pre && aux 0
+end
+
 type dll_handle
 type dll_address
 type dll_mode = For_checking | For_execution
