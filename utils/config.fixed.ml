de8596aafeb25ca7a6f2c38c8546cfa5f15e5e9f
David Allsopp <david.allsopp@metastack.com>
2021-09-24 11:42:16 +0100

    Make Windows header absolute (as on Unix)

--- utils/config.fixed.ml
+++ utils/config.fixed.ml
@@ -84,5 +84,5 @@ let systhread_supported = false
 let flexdll_dirs = []
 let ar_supports_response_files = true
 let shebangscripts = false
-let suffixing = false
+let suffixing = true
 let launch_method = "sh"
