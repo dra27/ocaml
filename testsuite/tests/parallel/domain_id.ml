293a6d1ba95d098935c6734718b40a1148143327
Xavier Leroy <xavier.leroy@college-de-france.fr>
2022-02-10 16:19:00 +0100

    Join all domains before exiting
    Avoids crashes in cleanup-at-exit mode.

--- testsuite/tests/parallel/domain_id.ml
+++ testsuite/tests/parallel/domain_id.ml
@@ -28,7 +28,8 @@ let test_different_ids () =
   assert (get_id d1 <> get_id d2);
   join d1; join d2;
   let d3 = Domain.spawn id in
-  assert (get_id d1 <> get_id d3)
+  assert (get_id d1 <> get_id d3);
+  join d3
 
 
 let () =
