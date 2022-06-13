b99063b1b0050344be24b4091d877b5bcb7bc83d
Florian Angeletti <florian.angeletti@inria.fr>
2022-06-13 17:43:18 +0200

    Merge pull request PR#10926 from dra27/check-symbols
    Ensure C global symbols are consistently prefixed

--- otherlibs/unix/fork.c
+++ otherlibs/unix/fork.c
@@ -35,7 +35,7 @@ void caml_atfork_child() {
 }
 #endif
 
-CAMLprim value unix_fork(value unit)
+CAMLprim value caml_unix_fork(value unit)
 {
   int ret;
 #if 0
@@ -48,10 +48,18 @@ CAMLprim value unix_fork(value unit)
   CAML_EV_FLUSH();
 
   ret = fork();
+<<<<<<< HEAD
 #if 0
   if (ret == 0) caml_atfork_hook();
 #endif
   if (ret == -1) uerror("fork", Nothing);
+||||||| parent of 6887832153 (Merge pull request PR#10926 from dra27/check-symbols)
+
+  if (ret == -1) uerror("fork", Nothing);
+=======
+
+  if (ret == -1) caml_uerror("fork", Nothing);
+>>>>>>> 6887832153 (Merge pull request PR#10926 from dra27/check-symbols)
 
 #if 0
   if (ret == 0) {
--- otherlibs/unix/signals.c
+++ otherlibs/unix/signals.c
@@ -63,7 +63,7 @@ static value encode_sigset(sigset_t * set)
 
 static int sigprocmask_cmd[3] = { SIG_SETMASK, SIG_BLOCK, SIG_UNBLOCK };
 
-CAMLprim value unix_sigprocmask(value vaction, value vset)
+CAMLprim value caml_unix_sigprocmask(value vaction, value vset)
 {
   int how;
   sigset_t set, oldset;
@@ -78,14 +78,23 @@ CAMLprim value unix_sigprocmask(value vaction, value vset)
   retcode = caml_sigmask_hook(how, &set, &oldset);
   caml_leave_blocking_section();
   /* Run any handlers for just-unmasked pending signals */
+<<<<<<< HEAD
   caml_process_pending_signals();
   if (retcode != 0) unix_error(retcode, "sigprocmask", Nothing);
+||||||| parent of 6887832153 (Merge pull request PR#10926 from dra27/check-symbols)
+  caml_process_pending_actions();
+  if (retcode != 0) unix_error(retcode, "sigprocmask", Nothing);
+=======
+  caml_process_pending_actions();
+  if (retcode != 0) caml_unix_error(retcode, "sigprocmask", Nothing);
+>>>>>>> 6887832153 (Merge pull request PR#10926 from dra27/check-symbols)
   return encode_sigset(&oldset);
 }
 
-CAMLprim value unix_sigpending(value unit)
+CAMLprim value caml_unix_sigpending(value unit)
 {
   sigset_t pending;
+<<<<<<< HEAD
   int i;
   if (sigpending(&pending) == -1) uerror("sigpending", Nothing);
   for (i = 1; i < NSIG; i++)
@@ -94,10 +103,35 @@ CAMLprim value unix_sigpending(value unit)
 #endif
     if(caml_pending_signals[i])
       sigaddset(&pending, i);
+||||||| parent of 6887832153 (Merge pull request PR#10926 from dra27/check-symbols)
+  int i, j;
+  uintnat curr;
+  if (sigpending(&pending) == -1) uerror("sigpending", Nothing);
+  for (i = 0; i < NSIG_WORDS; i++) {
+    curr = atomic_load(&caml_pending_signals[i]);
+    if (curr == 0) continue;
+    for (j = 0; j < BITS_PER_WORD; j++) {
+      if (curr & ((uintnat)1 << j))
+      sigaddset(&pending, i * BITS_PER_WORD + j + 1);
+    }
+  }
+=======
+  int i, j;
+  uintnat curr;
+  if (sigpending(&pending) == -1) caml_uerror("sigpending", Nothing);
+  for (i = 0; i < NSIG_WORDS; i++) {
+    curr = atomic_load(&caml_pending_signals[i]);
+    if (curr == 0) continue;
+    for (j = 0; j < BITS_PER_WORD; j++) {
+      if (curr & ((uintnat)1 << j))
+      sigaddset(&pending, i * BITS_PER_WORD + j + 1);
+    }
+  }
+>>>>>>> 6887832153 (Merge pull request PR#10926 from dra27/check-symbols)
   return encode_sigset(&pending);
 }
 
-CAMLprim value unix_sigsuspend(value vset)
+CAMLprim value caml_unix_sigsuspend(value vset)
 {
   sigset_t set;
   int retcode;
@@ -105,19 +139,19 @@ CAMLprim value unix_sigsuspend(value vset)
   caml_enter_blocking_section();
   retcode = sigsuspend(&set);
   caml_leave_blocking_section();
-  if (retcode == -1 && errno != EINTR) uerror("sigsuspend", Nothing);
+  if (retcode == -1 && errno != EINTR) caml_uerror("sigsuspend", Nothing);
   return Val_unit;
 }
 
 #else
 
-CAMLprim value unix_sigprocmask(value vaction, value vset)
+CAMLprim value caml_unix_sigprocmask(value vaction, value vset)
 { caml_invalid_argument("Unix.sigprocmask not available"); }
 
-CAMLprim value unix_sigpending(value unit)
+CAMLprim value caml_unix_sigpending(value unit)
 { caml_invalid_argument("Unix.sigpending not available"); }
 
-CAMLprim value unix_sigsuspend(value vset)
+CAMLprim value caml_unix_sigsuspend(value vset)
 { caml_invalid_argument("Unix.sigsuspend not available"); }
 
 #endif
