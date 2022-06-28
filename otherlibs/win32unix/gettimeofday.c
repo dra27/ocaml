5a72d63ff7f3fc567d631e6b51616099a35f87c4
David Allsopp <david.allsopp@metastack.com>
2022-06-28 11:28:54 +0100

    Remove invalid pointer cast in Unix.gettimeofday
    Windows FILETIME structures are 32-bit aligned so a pointer shouldn't be
    directly cast to a uint64_t.

--- otherlibs/win32unix/gettimeofday.c
+++ otherlibs/win32unix/gettimeofday.c
@@ -26,6 +26,7 @@ double unix_gettimeofday_unboxed(value unit)
 {
   FILETIME ft;
   double tm;
+  ULARGE_INTEGER utime;
   GetSystemTimeAsFileTime(&ft);
 #if defined(_MSC_VER) && _MSC_VER < 1300
   /* This compiler can't cast uint64_t to double! Fortunately, this doesn't
@@ -34,7 +35,9 @@ double unix_gettimeofday_unboxed(value unit)
    */
   tm = *(int64_t *)&ft - epoch_ft; /* shift to Epoch-relative time */
 #else
-  tm = *(uint64_t *)&ft - epoch_ft; /* shift to Epoch-relative time */
+  utime.LowPart = ft.dwLowDateTime;
+  utime.HighPart = ft.dwHighDateTime;
+  tm = utime.QuadPart - epoch_ft; /* shift to Epoch-relative time */
 #endif
   return (tm * 1e-7);  /* tm is in 100ns */
 }
