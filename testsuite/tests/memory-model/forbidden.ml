e04119a436e59bbb5c06a893e1b0ea465ba42436
Xavier Leroy <xavier.leroy@college-de-france.fr>
2022-07-11 19:00:06 +0200

    tests/memory-model: disable entirely on BSD systems
    The FreeBSD and OpenBSD hosts used for Jenkins CI often take 20 minutes
    to complete these tests, probably because of poor scheduling of the
    busy-waiting barriers.  We have plenty of non-BSD hosts to test the
    memory model.

--- testsuite/tests/memory-model/forbidden.ml
+++ testsuite/tests/memory-model/forbidden.ml
@@ -1,7 +1,14 @@
 (* TEST
   modules="opt.ml barrier.ml hist.ml shared.ml run.ml outcome.ml"
+<<<<<<< HEAD
   * skip
   reason = "OCaml 5 only"
+||||||| parent of b3b51a7864 (tests/memory-model: disable entirely on BSD systems)
+=======
+  * not-bsd
+  ** bytecode
+  ** native
+>>>>>>> b3b51a7864 (tests/memory-model: disable entirely on BSD systems)
 *)
 
 (* Memory model test:
