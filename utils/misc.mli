5f92e587acb2583021f78147a3b0504ed6a2b58f
David Allsopp <david.allsopp@metastack.com>
2022-08-17 11:10:30 +0100

    Add --enable-runtime-search configuration option
    --enable-runtime-search=[no|yes|always] adds new features to the
    launcher used for bytecode executables which do not embed their own
    runtime. By default, the header continues to behave as before - the
    launcher will attempt to start the runtime using the absolute path which
    the compiler was configured with.
    
    The new search mode will then search for the runtime first in the
    directory containing the running executable and then in PATH.
    
    XXX At present, only headernt.c has been updated, this PR assumes the
        overhaul of header.c and headernt.c will be merged first.

--- utils/misc.mli
+++ utils/misc.mli
@@ -488,3 +488,11 @@ module EnvLazy: sig
   val backtrack : log -> unit
 
 end
+
+module RuntimeID : sig
+  (** Manipulation of the Runtime ID values used for relocating the compiler *)
+
+  val make_zinc : static:bool -> int31:bool -> int -> is_release:bool -> string
+  (** [make_zinc static int31 release_number is_release] returns the Zinc
+      RuntimeID for the given parameters *)
+end
