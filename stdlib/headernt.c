ad12b77ad51a284e6ed29521746dd2d26c079fd1
David Allsopp <david.allsopp@metastack.com>
2022-08-17 09:08:03 +0100

    Merge the bytecode executable headers
    camlheader no longer contains the full name of the runtime, instead the
    first line begins #! (if shebang scripts are supported) or !# (to
    indicate that an executable header follows). The rest of the line is the
    directory containing the runtime files, which is used by ocamlc to
    append the actual name of the runtime variant.
    
    The executable header no longer contains any default path, and RNTM is
    always required. The RNTM section is now only written when using an
    executable header; tools/ocamlsize is updated to read display the shebang
    line as the runtime; its "(default runtime)" is therefore corrected to
    display "(custom runtime)" for `-custom` executables.

--- stdlib/headernt.c
+++ stdlib/headernt.c
@@ -31,8 +31,6 @@
 #endif
 #endif
 
-char * default_runtime_name = RUNTIME_NAME;
-
 static
 #ifdef _MSC_VER
 __forceinline
@@ -70,12 +68,11 @@ static __inline char * read_runtime_path(HANDLE h)
     } else if (path_size > 0)
       ofs += read_size(buffer + 4);
   }
-  if (path_size == 0) return default_runtime_name;
+  if (path_size == 0) return NULL;
   if (path_size >= MAX_PATH) return NULL;
   if (SetFilePointer(h, -ofs, NULL, FILE_END) == -1) return NULL;
   if (! ReadFile(h, runtime_path, path_size, &nread, NULL)) return NULL;
   if (nread != path_size) return NULL;
-  runtime_path[path_size - 1] = 0;
   return runtime_path;
 }
 
