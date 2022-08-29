c0b52f06510ce5205ce76e2170f9e5a809d631a8
David Allsopp <david.allsopp@metastack.com>
2022-08-29 09:39:49 +0100

    Add Zinc RuntimeID Mask for portability

--- bytecomp/bytelink.ml
+++ bytecomp/bytelink.ml
@@ -310,7 +310,7 @@ let write_header outchan =
       let ocamlrun =
         Printf.sprintf "ocamlrun%s-%s"
                        !Clflags.runtime_variant
-                       Config.bytecode_runtime_id
+                       Config.zinc_runtime_id
       in
       (false, ocamlrun)
   in
