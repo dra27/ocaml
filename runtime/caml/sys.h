dcf900278cbb9333bf89cfdd0e3f07c6bfd4fa1b
David Allsopp <david.allsopp@metastack.com>
2022-09-01 12:07:42 +0100

    Add Sys.interpreter
    In native code, Sys.interpreter is simply Sys.executable_name, but in
    bytecode it is the path to the actual interpreter (which will only be
    Sys.executable_name for -custom or -output-complete-* programs)

--- runtime/caml/sys.h
+++ runtime/caml/sys.h
@@ -33,7 +33,7 @@ CAMLnoret CAMLextern void caml_sys_error (value);
 CAMLnoret CAMLextern void caml_sys_io_error (value);
 
 CAMLextern double caml_sys_time_unboxed(value);
-CAMLextern void caml_sys_init (char_os * exe_name, char_os ** argv);
+CAMLextern void caml_sys_init (char_os *, char_os *, char_os ** argv);
 CAMLextern void caml_locate_standard_library (const char_os *);
 
 CAMLnoret CAMLextern void caml_do_exit (int);
