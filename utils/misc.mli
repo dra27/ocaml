51b54bb5f3a45af1fddb27a0bde73bfa7eded1e8
David Allsopp <david.allsopp@metastack.com>
2022-09-04 22:14:11 +0100

    Compute the Zinc runtime ID
    WIP work towards the use int31 bit (not completed) and the
    additional_runtimes setting is present, although it's to be removed.

--- utils/misc.mli
+++ utils/misc.mli
@@ -826,6 +826,14 @@ module Utf8_lexeme: sig
       are not checked. *)
 end
 
+module RuntimeID : sig
+  (** Manipulation of the Runtime ID values used for relocating the compiler *)
+
+  val make_zinc : static:bool -> int31:bool -> int -> is_release:bool -> string
+  (** [make_zinc static int31 release_number is_release] returns the Zinc
+      RuntimeID for the given parameters *)
+end
+
 (** {1 Miscellaneous type aliases} *)
 
 type filepath = string
