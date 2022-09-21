a9ae3c954d70b5840c33a90c1149bee828c7a7a0
Gabriel Scherer <gabriel.scherer@gmail.com>
2022-09-21 17:01:29 +0200

    Domain.recommended_domain_count with type unit -> int (PR#11545)
    Presenting this as a function rather than a constant in the stdlib
    leaves us with more implementation options open in the future.

--- testsuite/tests/parallel/recommended_domain_count.ml
+++ testsuite/tests/parallel/recommended_domain_count.ml
@@ -7,6 +7,6 @@ reason = "OCaml 5 only"
 external get_max_domains : unit -> int = "caml_get_max_domains"
 
 let _ =
-  assert (Domain.recommended_domain_count > 0);
-  assert (Domain.recommended_domain_count <= (get_max_domains ()));
+  assert (Domain.recommended_domain_count () > 0);
+  assert (Domain.recommended_domain_count () <= (get_max_domains ()));
   print_string "passed\n"
