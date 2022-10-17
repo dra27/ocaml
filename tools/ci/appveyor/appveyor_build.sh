96241fdee41a0ee792bddd7d5e11363cadb760a7
David Allsopp <david.allsopp@metastack.com>
2022-10-17 12:25:05 +0100

    AppVeyor script fixes for Cygwin
    (cherry picked from commit 94f839137cdf007412c7fd13e7279db48baf58fb)

--- tools/ci/appveyor/appveyor_build.sh
+++ tools/ci/appveyor/appveyor_build.sh
@@ -91,14 +91,19 @@ OCAMLROOT=$(echo "$OCAMLROOT" | cygpath -f - -m)
 
 if [[ $BOOTSTRAP_FLEXDLL = 'false' ]] ; then
   case "$PORT" in
-    cygwin*) ;;
-    *) export PATH="$FLEXDLLROOT:$PATH";;
+    cygwin*)
+      install_flexdll='false';;
+    *)
+      export PATH="$FLEXDLLROOT:$PATH"
+      install_flexdll='true';;
   esac
+else
+  install_flexdll='false'
 fi
 
 case "$1" in
   install)
-    if [[ $BOOTSTRAP_FLEXDLL = 'false' ]] ; then
+    if [[ $install_flexdll = 'true' ]] ; then
       mkdir -p "$FLEXDLLROOT"
       cd "$APPVEYOR_BUILD_FOLDER/../flexdll"
       # The objects are always built from the sources
@@ -151,7 +156,7 @@ case "$1" in
   *)
     cd "$APPVEYOR_BUILD_FOLDER/../$BUILD_PREFIX-$PORT"
 
-    if [[ $BOOTSTRAP_FLEXDLL = 'false' ]] ; then
+    if [[ $install_flexdll = 'true' ]] ; then
       tar -xzf "$APPVEYOR_BUILD_FOLDER/flexdll.tar.gz"
       cd "flexdll-$FLEXDLL_VERSION"
       $MAKE MSVC_DETECT=0 CHAINS=${PORT%32} support
