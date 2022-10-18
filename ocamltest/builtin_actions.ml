800190e41e6fc7596ac355d43e010c3d36231429
David Allsopp <david.allsopp@metastack.com>
2022-10-18 09:45:12 +0100

    Regard Cygwin as a BSD in ocamltest
    Sort of true! Mainly to disable the memory model tests, though.

--- ocamltest/builtin_actions.ml
+++ ocamltest/builtin_actions.ml
@@ -110,7 +110,7 @@ let not_windows = make
 
 let is_bsd_system s =
   match s with
-  | "bsd_elf" | "netbsd" | "freebsd" | "openbsd" -> true
+  | "bsd_elf" | "netbsd" | "freebsd" | "openbsd" | "cygwin" -> true
   | _ -> false
 
 let bsd = make
