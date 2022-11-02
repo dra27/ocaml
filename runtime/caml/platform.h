9c630bebee213a266d6414dd5085b9fa8fca6385
David Allsopp <david.allsopp@metastack.com>
2022-11-02 13:44:28 +0000

    Introduce caml_sys_mmap_alignment
    On Unix platforms, equal caml_sys_pagesize but on Windows reflects the
    fact that VirtualAlloc has a stricter alignment guarantee.

--- runtime/caml/platform.h
+++ runtime/caml/platform.h
@@ -128,9 +128,10 @@ void caml_plat_cond_free(caml_plat_cond*);
 
 uintnat caml_mem_round_up_pages(uintnat size);
 /* The size given to caml_mem_map and caml_mem_commit must be a multiple of
-   caml_plat_mmap_granularity. The size given to caml_mem_unmap and
-   caml_mem_decommit must match the size given to caml_mem_map/caml_mem_commit
-   for mem. */
+   caml_sys_pagesize. The size given to caml_mem_unmap and caml_mem_decommit
+   must match the size given to caml_mem_map/caml_mem_commit for mem. The
+   alignment given to caml_mem_map must not be greater than
+   caml_sys_mmap_alignment on Windows and Cygwin. */
 void* caml_mem_map(uintnat size, uintnat alignment, int reserve_only);
 void* caml_mem_commit(void* mem, uintnat size);
 void caml_mem_decommit(void* mem, uintnat size);
@@ -179,9 +180,8 @@ Caml_inline void caml_plat_unlock(caml_plat_mutex* m)
   check_err("unlock", pthread_mutex_unlock(m));
 }
 
-/* On Windows, the SYSTEM_INFO.dwPageSize is a DWORD (32-bit), but conveniently
-   long is also 32-bit */
-extern intnat caml_plat_mmap_granularity;
+extern intnat caml_sys_pagesize;
+extern intnat caml_sys_mmap_alignment;
 
 #endif /* CAML_INTERNALS */
 
