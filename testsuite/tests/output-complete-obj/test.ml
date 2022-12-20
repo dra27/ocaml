7ba5a4f01baa093b3158d6ea78be773751edf0ab
David Allsopp <david.allsopp@metastack.com>
2022-12-20 12:01:01 +0000

    Use ${cflags} in output-complete-obj tests
    Explicitly compiles the object using the C compiler (which inherits
    internal flags) rather than passing it to flexlink.
    
    This is likely to be a good change - passing a C file to flexlink
    includes no flags whatsoever.

--- testsuite/tests/output-complete-obj/test.ml
+++ testsuite/tests/output-complete-obj/test.ml
@@ -7,10 +7,12 @@ readonly_files = "test.ml_stub.c"
 flags = "-w -a -output-complete-obj"
 program = "test.ml.bc.${objext}"
 *** script
+script = "${cc} ${cflags} -c -I${ocamlsrcdir}/runtime -I${ocamlsrcdir}/runtime/winpthreads/include test.ml_stub.c"
+**** script
 script = "${mkexe} -I${ocamlsrcdir}/runtime -o test.ml_bc_stub.exe \
-                   test.ml.bc.${objext} ${nativecc_libs} test.ml_stub.c"
+                   test.ml.bc.${objext} ${nativecc_libs} test.ml_stub.${objext}"
 output = "${compiler_output}"
-**** run
+***** run
 program = "./test.ml_bc_stub.exe"
 stdout = "program-output"
 stderr = "program-output"
@@ -19,10 +21,12 @@ stderr = "program-output"
 flags = "-w -a -output-complete-obj"
 program = "test.ml.exe.${objext}"
 *** script
+script = "${cc} ${cflags} -c -I${ocamlsrcdir}/runtime -I${ocamlsrcdir}/runtime/winpthreads/include test.ml_stub.c"
+**** script
 script = "${mkexe} -I${ocamlsrcdir}/runtime -o test.ml_stub.exe \
-                   test.ml.exe.${objext} ${bytecc_libs} test.ml_stub.c"
+                   test.ml.exe.${objext} ${bytecc_libs} test.ml_stub.${objext}"
 output = "${compiler_output}"
-**** run
+***** run
 program = "./test.ml_stub.exe"
 stdout = "program-output"
 stderr = "program-output"
