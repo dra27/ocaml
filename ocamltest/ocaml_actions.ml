4ba93c4be1af99a78579e2d7fa4d1f89d23d74f7
David Allsopp <david.allsopp@metastack.com>
2022-12-20 11:54:25 +0000

    Expose cflags variable in ocamltest

--- ocamltest/ocaml_actions.ml
+++ ocamltest/ocaml_actions.ml
@@ -1091,6 +1091,7 @@ let config_variables _log env =
       Sys.getenv_with_default_value "MKEXE" Ocamltest_config.mkexe;
     Ocaml_variables.c_preprocessor, Ocamltest_config.c_preprocessor;
     Ocaml_variables.cc, Ocamltest_config.cc;
+    Ocaml_variables.cflags, Ocamltest_config.cflags;
     Ocaml_variables.csc, Ocamltest_config.csc;
     Ocaml_variables.csc_flags, Ocamltest_config.csc_flags;
     Ocaml_variables.shared_library_cflags,
