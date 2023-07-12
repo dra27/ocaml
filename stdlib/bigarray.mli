ebb39943ce08d3dc34dedcdfaf3a849bd152f003
Xavier Leroy <xavierleroy@users.noreply.github.com>
2023-07-12 11:59:06 +0200

    Merge pull request PR#12357 from xavierleroy/float16-at-end
    Move constructor `Bigarray.Float16` last in the type `Bigarray.kind`

--- stdlib/bigarray.mli
+++ stdlib/bigarray.mli
@@ -120,8 +120,8 @@ type ('a, 'b) kind =
   | Nativeint : (nativeint, nativeint_elt) kind
   | Complex32 : (Complex.t, complex32_elt) kind
   | Complex64 : (Complex.t, complex64_elt) kind
-  | Char : (char, int8_unsigned_elt) kind (**)
-  | Float16 : (float, float16_elt) kind
+  | Char : (char, int8_unsigned_elt) kind
+  | Float16 : (float, float16_elt) kind  (**)
 (** To each element kind is associated an OCaml type, which is
    the type of OCaml values that can be stored in the Bigarray
    or read back from it.  This type is not necessarily the same
