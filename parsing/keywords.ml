5663fc6a561ba96b03c518e69a30c4d9dd8d58e6
David Allsopp <david.allsopp@metastack.com>
2023-07-19 21:15:05 +0100

    Separate keywords table from the Lexer
    Lexer.is_keyword was exposed as part of the support for raw identifier
    syntax in OCaml 5.2. Using this function directly added a dependency
    for Path and Pprintast on Lexer and transitively on Parser which did not
    exist before. Unfortunately, this significantly reduces the parallelism
    when compiling ocamlcommon.cmxa.
    
    The fix here is to move the keyword_table from Lexer to a new module
    Keywords and instead expose Keywords.is_keyword and additionally
    Keywords.token_of_string for the Lexer itself.
    
    There is a small wart that Parser.token is required, but can't be
    directly referenced because this would create a dependency for the new
    keywords.cmx on parser.cmx and undermine the fix. The solution is to
    generate an additional mli-only module Keywords_token containing a copy
    (with type equality) of Parser.token and use that.

---
+++ parsing/keywords.ml
@@ -0,0 +1,84 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
+(*                                                                        *)
+(*   Copyright 1996 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(* The table of keywords *)
+
+open Keywords_token
+
+let keyword_table =
+  Misc.create_hashtable 149 [
+    "and", AND;
+    "as", AS;
+    "assert", ASSERT;
+    "begin", BEGIN;
+    "class", CLASS;
+    "constraint", CONSTRAINT;
+    "do", DO;
+    "done", DONE;
+    "downto", DOWNTO;
+    "else", ELSE;
+    "end", END;
+    "exception", EXCEPTION;
+    "external", EXTERNAL;
+    "false", FALSE;
+    "for", FOR;
+    "fun", FUN;
+    "function", FUNCTION;
+    "functor", FUNCTOR;
+    "if", IF;
+    "in", IN;
+    "include", INCLUDE;
+    "inherit", INHERIT;
+    "initializer", INITIALIZER;
+    "lazy", LAZY;
+    "let", LET;
+    "match", MATCH;
+    "method", METHOD;
+    "module", MODULE;
+    "mutable", MUTABLE;
+    "new", NEW;
+    "nonrec", NONREC;
+    "object", OBJECT;
+    "of", OF;
+    "open", OPEN;
+    "or", OR;
+(*  "parser", PARSER; *)
+    "private", PRIVATE;
+    "rec", REC;
+    "sig", SIG;
+    "struct", STRUCT;
+    "then", THEN;
+    "to", TO;
+    "true", TRUE;
+    "try", TRY;
+    "type", TYPE;
+    "val", VAL;
+    "virtual", VIRTUAL;
+    "when", WHEN;
+    "while", WHILE;
+    "with", WITH;
+
+    "lor", INFIXOP3("lor"); (* Should be INFIXOP2 *)
+    "lxor", INFIXOP3("lxor"); (* Should be INFIXOP2 *)
+    "mod", INFIXOP3("mod");
+    "land", INFIXOP3("land");
+    "lsl", INFIXOP4("lsl");
+    "lsr", INFIXOP4("lsr");
+    "asr", INFIXOP4("asr")
+]
+
+let is_keyword = Hashtbl.mem keyword_table
+
+let token_of_string = Hashtbl.find keyword_table
