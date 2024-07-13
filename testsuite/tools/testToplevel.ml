27c0003fae98256aa4f4ce20616c920248d7d475
David Allsopp <david.allsopp@metastack.com>
2024-07-13 21:29:10 +0200

    Update the installation test for enable-relative

--- testsuite/tools/testToplevel.ml
+++ testsuite/tools/testToplevel.ml
@@ -98,7 +98,7 @@ let run config env mode =
         ~fails:(expected_exit_code <> 0)
         ~runtime:(mode = Bytecode && not config.launcher_searches_for_ocamlrun)
         ~stubs:(mode = Bytecode && has_c_stubs)
-        ~stdlib:true env toplevel args
+        ~stdlib:(config.has_relative_libdir = None) env toplevel args
     in
     Environment.display_output output;
     if exit_code <> expected_exit_code then
