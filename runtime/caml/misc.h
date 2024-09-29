671786e1089010eff449fdef6fdeb30b6bc3bd62
David Allsopp <david.allsopp@metastack.com>
2024-09-29 11:44:12 +0100

    Correct OOM semantics of caml_stat_strdup_to_os
    The Windows implementation caml_stat_strdup_to_utf16 returned NULL on
    OOM, where caml_stat_strdup raises Out_of_memory. Windows implementation
    fixed to raise Out_of_memory.

--- runtime/caml/misc.h
+++ runtime/caml/misc.h
@@ -447,7 +447,6 @@ extern double caml_log1p(double);
 #define caml_stat_strconcat_os caml_stat_wcsconcat
 
 #define caml_stat_strdup_to_os caml_stat_strdup_to_utf16
-#define caml_stat_strdup_noexc_to_os caml_stat_strdup_noexc_to_utf16
 #define caml_stat_strdup_of_os caml_stat_strdup_of_utf16
 #define caml_copy_string_of_os caml_copy_string_of_utf16
 
@@ -488,7 +487,6 @@ extern double caml_log1p(double);
 #define caml_stat_strconcat_os caml_stat_strconcat
 
 #define caml_stat_strdup_to_os caml_stat_strdup
-#define caml_stat_strdup_noexc_to_os caml_stat_strdup_noexc
 #define caml_stat_strdup_of_os caml_stat_strdup
 #define caml_copy_string_of_os caml_copy_string
 
