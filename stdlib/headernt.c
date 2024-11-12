79afc14470417e61e21cb9f74fcf7b41a992b09d
David Allsopp <david.allsopp@metastack.com>
2024-11-12 17:57:13 +0000

    Missing error case in headernt.c

--- stdlib/headernt.c
+++ stdlib/headernt.c
@@ -125,7 +125,9 @@ _Noreturn void __cdecl wmainCRTStartup(void)
   PROCESS_INFORMATION procinfo;
   DWORD retcode;
 
-  GetModuleFileName(NULL, truename, sizeof(truename)/sizeof(wchar_t));
+  if (GetModuleFileName(NULL, truename, sizeof(truename)/sizeof(wchar_t)) == 0)
+    exit_with_error(L"Out of memory", NULL, NULL);
+
   h = CreateFile(truename, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE,
                  NULL, OPEN_EXISTING, 0, NULL);
   if (h == INVALID_HANDLE_VALUE || !read_runtime_path(h, runtime_path) ||
