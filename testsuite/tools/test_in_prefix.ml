c4c7c587d662efeb6268b83ec2cfece9e5de0716
David Allsopp <david.allsopp@metastack.com>
2024-11-13 16:27:03 +0000

    Merge stdlib/header{,nt}.c

--- testsuite/tools/test_in_prefix.ml
+++ testsuite/tools/test_in_prefix.ml
@@ -2083,7 +2083,7 @@ let compile_test ~original env bindir libdir =
                     | Tendered ->
                         if argv0_not_ocaml then
                           if Sys.win32 then
-                            (* stdlib/headernt.c will find ocamlrun (because it
+                            (* stdlib/header.c will find ocamlrun (because it
                                effectively uses caml_executable_name) but fails
                                to hand off the bytecode image, which causes
                                ocamlrun to exit with code 127 *)
@@ -2096,7 +2096,7 @@ let compile_test ~original env bindir libdir =
                                Somewhat confusingly, it exits with code 2 *)
                             Fail 2
                         else if Sys.win32 then
-                          (* stdlib/headernt.c correctly preserves argv[0] *)
+                          (* stdlib/header.c correctly preserves argv[0] *)
                           Success {executable_name = test_program_path; argv0}
                         else
                           (* stdlib/header.c does not preserve argv[0] *)
