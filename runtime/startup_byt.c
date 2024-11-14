a3775f0623c92faf5bc5b65b8c94217d575b4e82
David Allsopp <david.allsopp@metastack.com>
2024-11-14 15:07:22 +0000

    Simplify the interface of caml_attempt_open
    Previously, caml_attempt_open received a pointer to the string and
    passed this string to caml_search_exe_in_path. This function allocates a
    fresh string which was then assigned to that pointer.
    
    This interface was a little cryptic to follow - in particular, there
    were several scenarios in which the result was not being freed. It's
    also not entirely clear that caml_attempt_open involved a PATH-search,
    which appears to have been applied in error when CAML_DEBUG_FILE was
    added.

--- runtime/startup_byt.c
+++ runtime/startup_byt.c
@@ -115,21 +115,18 @@ int caml_read_trailer(int fd, struct exec_trailer *trail)
       ? 0 : WRONG_MAGIC;
 }
 
-int caml_attempt_open(char_os **name, struct exec_trailer *trail,
+int caml_attempt_open(const char_os *name, struct exec_trailer *trail,
                       int do_open_script)
 {
-  char_os * truename;
   int fd;
   int err;
   char buf [2], * u8;
 
-  truename = caml_search_exe_in_path(*name);
-  u8 = caml_stat_strdup_of_os(truename);
+  u8 = caml_stat_strdup_of_os(name);
   CAML_GC_MESSAGE(STARTUP, "Opening bytecode executable %s\n", u8);
   caml_stat_free(u8);
-  fd = open_os(truename, O_RDONLY | O_BINARY);
+  fd = open_os(name, O_RDONLY | O_BINARY);
   if (fd == -1) {
-    caml_stat_free(truename);
     CAML_GC_MESSAGE(STARTUP, "Cannot open file\n");
     if (errno == EMFILE)
       return NO_FDS;
@@ -140,7 +137,6 @@ int caml_attempt_open(char_os **name, struct exec_trailer *trail,
     err = read (fd, buf, 2);
     if (err < 2 || (buf [0] == '#' && buf [1] == '!')) {
       close(fd);
-      caml_stat_free(truename);
       CAML_GC_MESSAGE(STARTUP, "Rejected #! script\n");
       return BAD_BYTECODE;
     }
@@ -148,11 +144,9 @@ int caml_attempt_open(char_os **name, struct exec_trailer *trail,
   err = caml_read_trailer(fd, trail);
   if (err != 0) {
     close(fd);
-    caml_stat_free(truename);
     CAML_GC_MESSAGE(STARTUP, "Not a bytecode executable\n");
     return err;
   }
-  *name = truename;
   return fd;
 }
 
@@ -469,7 +463,7 @@ CAMLexport void caml_main(char_os **argv)
   value res;
   char * req_prims;
   char_os * shared_lib_path, * shared_libs;
-  char_os * exe_name, * proc_self_exe, * argv0;
+  char_os * exe_name = NULL, * proc_self_exe, * argv0, * tofree = NULL;
 
   /* Determine options */
   caml_parse_ocamlrunparam();
@@ -494,8 +488,13 @@ CAMLexport void caml_main(char_os **argv)
 
   if (caml_byte_program_mode != APPENDED || proc_self_exe == NULL) {
     /* First, try argv[0] (when ocamlrun is called by a bytecode program) */
-    exe_name = argv[0];
-    fd = caml_attempt_open(&exe_name, &trail, 0);
+    exe_name = caml_search_exe_in_path(argv[0]);
+    fd = caml_attempt_open(exe_name, &trail, 0);
+    if (proc_self_exe == NULL) {
+      tofree = argv0 = exe_name;
+    } else if (fd < 0) {
+      caml_stat_free(exe_name);
+    }
   }
 
   /* Little grasshopper wonders why we do that at all, since
@@ -507,7 +506,7 @@ CAMLexport void caml_main(char_os **argv)
   if (caml_byte_program_mode == APPENDED || fd < 0) {
     if (proc_self_exe != NULL) {
       exe_name = proc_self_exe;
-      fd = caml_attempt_open(&exe_name, &trail, 0);
+      fd = caml_attempt_open(exe_name, &trail, 0);
     }
     if (fd < 0 && caml_byte_program_mode == APPENDED)
       error("unable to open file '%s'", caml_stat_strdup_of_os(exe_name));
@@ -529,8 +528,8 @@ CAMLexport void caml_main(char_os **argv)
     if (argv[pos] == 0) {
       error("no bytecode file specified");
     }
-    exe_name = argv[pos];
-    fd = caml_attempt_open(&exe_name, &trail, 1);
+    exe_name = caml_search_exe_in_path(argv[pos]);
+    fd = caml_attempt_open(exe_name, &trail, 1);
     switch(fd) {
     case FILE_NOT_FOUND:
       error("cannot find file '%s'",
@@ -556,6 +555,7 @@ CAMLexport void caml_main(char_os **argv)
 
   caml_standard_library_effective =
     caml_locate_standard_library(argv0, caml_standard_library_default, NULL);
+  caml_stat_free(tofree);
 
   /* Load the embedded overridden caml_standard_library_default value, if one is
      available. This value is set _after_ caml_standard_library_effective has
