ba40be7f85774023483ce7db5c1341da80c944bb
David Allsopp <david.allsopp@metastack.com>
2024-11-14 20:55:48 +0000

    header.c: handle parasitic case of argc < 1

--- stdlib/header.c
+++ stdlib/header.c
@@ -243,6 +243,9 @@ int main(int argc, char *argv[])
   char *truename, *runtime_path;
   int fd;
 
+  if (argc < 1)
+    exit_with_error("Unable to load bytecode image", NULL, NULL);
+
   truename = searchpath(argv[0]);
   fd = open(truename, O_RDONLY | O_BINARY);
   if (fd == -1 || (runtime_path = read_runtime_path(fd)) == NULL)
