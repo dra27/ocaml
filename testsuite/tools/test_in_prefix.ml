9d953c892791f06e446e74b38c3a5720d9eaafd4
David Allsopp <david.allsopp@metastack.com>
2024-11-21 20:49:23 +0000

    Use entrypoint flexdll branch
    Fixes loading unix.cmxs in Cygwin64

--- testsuite/tools/test_in_prefix.ml
+++ testsuite/tools/test_in_prefix.ml
@@ -1205,9 +1205,7 @@ let run (config : Installation.t) env mode =
       ["-noinit"; "-no-version"; "-noprompt"; "test_install_script.ml"]
     in
     let expected_exit_code =
-      if Sys.cygwin && mode = Native && List.mem "unix" libraries
-      || has_c_stubs && not Config.supports_shared_libraries then
-        (* cf. ocaml/flexdll#146 - Cygwin's ocamlnat can't load unix.cmxs *)
+      if has_c_stubs && not Config.supports_shared_libraries then
         125
       else
         0
@@ -1302,10 +1300,7 @@ let () =
       match expected_exit_code with
       | Some code -> code
       | None ->
-          if (Sys.cygwin && mode = Native && List.mem "unix" libraries)
-             || (not Config.supports_shared_libraries && has_c_stubs) then
-            (* cf. ocaml/flexdll#146 - Cygwin's natdynlink can't load
-                   unix.cmxs *)
+          if not Config.supports_shared_libraries && has_c_stubs then
             2
           else
             0
