bde6e782cdbdc818c51af34f1c1514fc37f83eba
David Allsopp <david.allsopp@metastack.com>
2024-12-09 15:57:14 +0100

    Add a `Config` entry for the compression libraries
    Define `Config.compression_c_libraries` for the flags needed to link
    with the `comprmarsh` library
    
    Need bootstrap
    
    Co-authored-by: Sébastien Hinderer <seb@tarides.com>
    (cherry picked from commit 5e3d5b97c4dfc696b8a0246da0bfe2a1087649db)

--- utils/config.mli
+++ utils/config.mli
@@ -97,6 +97,12 @@ val bytecomp_c_libraries: string
 val native_c_libraries: string
 (** The C libraries to link with native-code programs *)
 
+val compression_c_libraries: string
+(** The C libraries needed with -lcomprmarsh (should appear before
+    {!native_c_libraries} in a call to the C compiler)
+
+    @since 5.4 *)
+
 val native_pack_linker: string
 (** The linker to use for packaging (ocamlopt -pack) and for partial
     links (ocamlopt -output-obj). *)
