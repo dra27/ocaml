20ddc22c19f240644d0eab6d389d30f38d957eb6
David Allsopp <david.allsopp@metastack.com>
2025-01-26 09:51:55 +0000

    ld.conf-relative path interpretation
    Explicit relative paths in ld.conf are now interpreted relative to the
    directory containing ld.conf.

--- testsuite/tools/test_in_prefix.ml
+++ testsuite/tools/test_in_prefix.ml
@@ -1545,7 +1545,13 @@ let () =
              ocamlrun opens it in binary mode (the default) *)
           match test.stdlib with
           | "\r" :: _ when Sys.cygwin && lines <> [] ->
-              "\r" :: List.map (Fun.flip (^) "\r") (List.tl lines)
+              (* This all gets a bit silly until CRLF is consistently
+                 handled! *)
+              let lines =
+                "" :: List.take 2 (List.tl lines)
+                  @ ["."; ".."] @ List.drop 5 lines
+              in
+              List.map (Fun.flip (^) "\r") lines
           | _ ->
               lines
         in
@@ -1793,16 +1799,16 @@ let test_ld_conf env =
         "/", "/", None;
         "//", "//", None;
         (* Current and Parent directory names *)
-        ".", ".", None;
-        "..", "..", None;
+        ".", libdir / "", if_not_win32 ".\r";
+        "..", libdir / "..", if_not_win32 "..\r";
         (* Current and Parent directory names with OS-default trailing separator
            (i.e. ./ and ../ on Unix and .\ and ..\ on Windows) *)
-        "." / "", "." / "", None;
-        ".." / "", ".." / "", None;
+        "." / "", libdir / "", None;
+        ".." / "", libdir / ".." / "", None;
         (* "stublibs" relative to the Current and Parent directory (using OS-
            default separator) *)
-        "." / "stublibs", "." / "stublibs", None;
-        ".." / "stublibs", ".." / "stublibs", None;
+        "." / "stublibs", libdir / "stublibs", None;
+        ".." / "stublibs", libdir / ".." / "stublibs", None;
         (* Other cases - implicit and absolute entries, and entries beginning
            with the Current and Parent directory names *)
         "stublibs", "stublibs", None;
