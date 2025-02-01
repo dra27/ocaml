a01d781044b43bc6c8f05cd4b192cb86b2d92481
David Allsopp <david.allsopp@metastack.com>
2025-02-01 12:54:38 +0000

    Ignore empty strings in Compmisc.set_from_env
    Previously, invocations such as
    
    OCAML_COLOR= ocamlopt
    
    emitted warning 46 (bad-env-variable) which is not particularly helpful.
    At present, Compmisc.set_from_env is used to implement OCAML_COLOR and
    OCAML_ERROR_STYLE, neither of which attached significance to the empty
    string. Compmisc.set_from_env is there changed to explictly ignore the
    environment variable value if it is "".

--- driver/compmisc.mli
+++ driver/compmisc.mli
@@ -19,7 +19,9 @@ val reinit_path : ?auto_include:Load_path.auto_include_callback
     -> ?standard_library:string -> ?dir:string -> unit -> unit
 val initial_env : unit -> Env.t
 
-(* Support for flags that can also be set from an environment variable *)
+(* Support for flags that can also be set from an environment variable.
+   Environment variables which are set, but to the empty string are ignored
+   (i.e. they are treated as if they were not set at all) *)
 val set_from_env : 'a option ref -> 'a Clflags.env_reader -> unit
 val read_clflags_from_env : unit -> unit
 
