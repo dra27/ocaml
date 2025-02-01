110f3c9f427cdca9bd2b9c567e18aa1715d22c32
David Allsopp <david.allsopp@metastack.com>
2025-02-01 16:03:18 +0000

    Ignore empty components in PATH-like things
    POSIX recognises empty components in a PATH-like variable as meaning "."
    (the current directory). This is reflected in the processing of
    OCAMLTOP_INCLUDE_PATH, CAML_LD_LIBRARY_PATH and ld.conf where either a
    blank component or a blank line is interpreted as "."
    
    Somewhat confusingly, this processing is applied inconsistently between
    Unix and Windows (it's confusing given that Windowsm more readily
    includes the current working directory by default in PATH searches).
    
    It also has the side-effect that a "Set But Null" environment variable
    is interpreted as "." which counter-intuitively makes
    CAML_LD_LIBRARY_PATH= ocamlrun add the current working directory to the
    search path.
    
    Blank lines and empty components of both OCAMLTOP_INCLUDE_PATH and
    CAML_LD_LIBRARY_PATH are now ignored. The current working directory can
    still be explicitly included, of course, by adding a "." entry/line
    where required.

--- runtime/dynlink.c
+++ runtime/dynlink.c
@@ -170,10 +170,12 @@ CAMLexport char_os * caml_parse_ld_conf(const char_os * stdlib,
           while (q > p && *(q - 1) == '\r')
             q--;
         }
-        *q = '\0';
-        char_os *entry = expand_entry(libroot, p);
-        length += strlen_os(entry) + 1;
-        caml_ext_table_add(&entries, entry);
+        if (p < q) {
+          *q = '\0';
+          char_os *entry = expand_entry(libroot, p);
+          length += strlen_os(entry) + 1;
+          caml_ext_table_add(&entries, entry);
+        }
         p = r;
       }
 
