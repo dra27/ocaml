41636ca435e2a935c806ad4d81c0259828b54deb
David Allsopp <david.allsopp@metastack.com>
2025-02-01 14:13:54 +0000

    Ignore blank OCAMLRUNPARAM, OCAMLLIB and CAMLLIB
    Previously, if OCAMLRUNPARAM was "Set But Null", CAMLRUNPARAM was
    completely ignored. Similarly, a "Set But Null" value for OCAMLLIB
    caused CAMLLIB and subsequently the default location of the Standard
    Library to be ignored. Especially for OCAMLLIB/CAMLLIB, this behaviour
    is counter-intuitive, as it's highly unlikely to be useful.
    
    Now, if OCAMLLIB is set, but to the empty string, then CAMLLIB is
    checked and, more importantly, if it is either not set or also set to
    the empty string then the default location of the Standard Library is
    still used. For consistency, if OCAMLRUNPARAM is set, but to the empty string,
    then CAMLRUNPARAM is read.

--- testsuite/tools/test_ld_conf.ml
+++ testsuite/tools/test_ld_conf.ml
@@ -228,22 +228,31 @@ let tests _config env =
             outcome = outcome_caml_ld_library_path
                         @ if_ld_conf_found ["ld.conf"]} :: tests in
     let tests =
-      (* An empty CAMLLIB should cause ld.conf in the Standard Library to be
-         ignored, but not CAML_LD_LIBRARY PATH *)
+      (* An empty CAMLLIB should not stop ld.conf in the Standard Library being
+         processed *)
       {base with description = "Empty CAMLLIB";
                  caml_ld_library_path = Set ["env"];
                  camllib = Empty;
-                 stdlib = ["masked-stdlib"];
-                 outcome = ["env"]} :: tests in
+                 stdlib = ["stdlib"];
+                 outcome = "env" :: if_ld_conf_found ["stdlib"]} :: tests in
     let tests =
-      (* An empty OCAMLLIB should cause ld.conf in both the Standard Library and
-         CAMLLIB to be ignored, but not CAML_LD_LIBRARY_PATH *)
-      {description = "Empty OCAMLLIB";
+      (* An empty OCAMLLIB should similarly not cause ld.conf in either the
+         Standard Library or CAMLLIB to be ignored *)
+      {description = "Empty OCAMLLIB not masking CAMLLIB";
        caml_ld_library_path = Set ["env"];
        ocamllib = Empty;
-       camllib = Set ["masked-camllib"];
-       stdlib = ["masked-stdlib"];
-       outcome = ["env"]} :: tests in
+       camllib = Set ["camllib"];
+       stdlib = ["stdlib"];
+       outcome = ["env"; "camllib"]} :: tests in
+    let tests =
+      (* An empty OCAMLLIB should similarly not cause ld.conf in either the
+         Standard Library or CAMLLIB to be ignored *)
+      {description = "Empty OCAMLLIB not masking Standard Library";
+       caml_ld_library_path = Set ["env"];
+       ocamllib = Empty;
+       camllib = Unset;
+       stdlib = ["stdlib"];
+       outcome = "env" :: if_ld_conf_found ["stdlib"]} :: tests in
     tests
   in
   (* Batch 3: load priority, embedded NUL characters, EOL-at-EOF, etc. *)
@@ -359,31 +368,6 @@ let () =
       in
       if code = 0 then
         let lines =
-          (* Known issue: Sys.getenv processes blank environment variables
-             differently from _wgetenv which in the tests will cause it load
-             ld.conf files. The tests have been written to allow for this by
-             having the lines which are _not_ expected to appear on Unix be
-             prefixed with "masked-". *)
-          if Sys.win32 then
-            if ((test.camllib = Empty
-                   && not (Environment.is_renamed env))
-                || test.ocamllib = Empty) then
-              let unmask s = not (String.starts_with ~prefix:"masked-" s) in
-              let lines' = List.filter unmask lines in
-              (* If Windows behaviour has been harmonised, then the filtered
-                 list of lines would be the same as the unfiltered list. If this
-                 happens, insert an extra line to "poison" the test output to
-                 prevent this behaviour from being silently fixed. *)
-              if lines = lines' then
-                "poisoned"::lines
-              else
-                lines'
-            else
-              lines
-          else
-            lines
-        in
-        let lines =
           (* Known issue: ocamlc opens ld.conf in text mode on Cygwin but
              ocamlrun opens it in binary mode (the default). This means that
              ocamlrun will return lines ending with \r, but ocamlc will both
@@ -394,7 +378,13 @@ let () =
              ocamlrun and ocamlc is harmonised. *)
           match test.stdlib with
           | "\r" :: _ when Sys.cygwin && lines <> [] ->
-              "\r" :: List.map (Fun.flip (^) "\r") (List.tl lines)
+              (* This all gets a bit silly until CRLF is consistently
+                 handled! *)
+              let lines =
+                "" :: List.take 2 (List.tl lines)
+                  @ ["."; ".."] @ List.drop 5 lines
+              in
+              List.map (Fun.flip (^) "\r") lines
           | _ ->
               lines
         in
