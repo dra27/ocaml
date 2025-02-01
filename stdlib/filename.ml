ed0ce03d3f03798fe6279097ca27f35f5f6677fd
David Allsopp <david.allsopp@metastack.com>
2025-02-01 12:35:23 +0000

    Ignore empty TMPDIR in the Filename module
    For Unix, the Filename module now treats TMPDIR being set to the
    empty string, as if it were not set at all (which causes it to use /tmp
    instead).

--- stdlib/filename.ml
+++ stdlib/filename.ml
@@ -114,7 +114,9 @@ module Unix : SYSDEPS = struct
       None
 
   let temp_dir_name =
-    try Sys.getenv "TMPDIR" with Not_found -> "/tmp"
+    match Sys.getenv_opt "TMPDIR" with
+    | None | Some "" -> "/tmp"
+    | Some dir -> dir
   let quote = generic_quote "'\\''"
   let quote_command cmd ?stdin ?stdout ?stderr args =
     String.concat " " (List.map quote (cmd :: args))
