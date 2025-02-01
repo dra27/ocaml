3192c036a3070aec38ce126e241cdb0516314974
David Allsopp <david.allsopp@metastack.com>
2025-02-01 16:03:18 +0000

    Ignore empty components in PATH-like things
    POSIX recognises empty components in a PATH-like variable as meaning "."
    (the current directory). This is reflected in the processing of
    OCAMLTOP_INCLUDE_PATH, CAML_LD_LIBRARY_PATH and ld.conf where either a
    blank component or a blank line is interpreted as "."
    
    Somewhat confusingly, this processing is applied inconsistently between
    Unix and Windows (it's confusing given that Windowsm more readily
    includes the current working directory by default in PATH searches).
    
    It also has the side-effect that a "Set But Null" environment variable
    is interpreted as "." which counter-intuitively makes
    CAML_LD_LIBRARY_PATH= ocamlrun add the current working directory to the
    search path.
    
    Blank lines and empty components of both OCAMLTOP_INCLUDE_PATH and
    CAML_LD_LIBRARY_PATH are now ignored. The current working directory can
    still be explicitly included, of course, by adding a "." entry/line
    where required.

--- bytecomp/dll.ml
+++ bytecomp/dll.ml
@@ -154,7 +154,6 @@ let ld_library_path_contents () =
   | exception Not_found ->
       []
   | s ->
-      (* NB: Misc.split_path_contents "" = [] *)
       Misc.split_path_contents s
 
 (* Initialization for separate compilation *)
