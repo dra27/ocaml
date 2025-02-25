b6429be276c4d050c0ad18821f1ea01ab8c38f3f
David Allsopp <david.allsopp@metastack.com>
2025-02-25 10:53:38 +0000

    Strip directory from Sys.argv.(0) in cmt files
    Cmt_format.cmt_infos.cmt_args includes a copy of Sys.argv from the
    compiler invocation. The user is in control of the arguments passed to a
    binary (Sys.argv.(1) and above) and can avoid embedding absolute paths,
    but Sys.argv.(0) on some platforms is converted to an absolute path,
    regardless of how the executable was invoked.

--- file_formats/cmt_format.ml
+++ file_formats/cmt_format.ml
@@ -476,12 +476,23 @@ let save_cmt target binary_annots initial_env cmi shape =
          let cmt_annots = clear_env binary_annots in
          let cmt_uid_to_decl = index_declarations cmt_annots in
          let source_digest = Option.map Digest.file sourcefile in
+         let cmt_args =
+           let base_argv0 =
+             (* Avoid embedding any absolute path in Sys.argv.(0) *)
+             let base_argv0 = Filename.basename Sys.argv.(0) in
+             if Sys.win32 && String.ends_with ~suffix:".exe" base_argv0 then
+               Filename.chop_extension base_argv0
+             else
+               base_argv0 in
+           let cmt_args = Array.copy Sys.argv in
+           cmt_args.(0) <- base_argv0;
+           cmt_args in
          let cmt = {
            cmt_modname = Unit_info.Artifact.modname target;
            cmt_annots;
            cmt_declaration_dependencies = !uids_deps;
            cmt_comments = Lexer.comments ();
-           cmt_args = Sys.argv;
+           cmt_args;
            cmt_sourcefile = sourcefile;
            cmt_builddir = Location.rewrite_absolute_path (Sys.getcwd ());
            cmt_loadpath = Load_path.get_paths ();
