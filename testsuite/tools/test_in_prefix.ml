8605902c16a18f05a15efecee2a0bf28b88153cb
David Allsopp <david.allsopp@metastack.com>
2025-03-16 15:27:29 +0000

    Share image fd between header.c and startup_byt.c
    The bytecode executable launcher (stdlib/header.c) can _only_ invoke
    ocamlrun after it has opened itself in order to find out the name of the
    runtime to execute. However, once ocamlrun is exec'd, the knowledge of
    this file was previously lost and if it could not be recovered from
    argv[0], then execution fails.
    
    This new approach, for both Windows and Unix, instead keeps the fd for
    the bytecode image open and passes its number to ocamlrun as
    __OCAML_EXEC_FD in the environment. ocamlrun detects this environment
    variable and uses that fd to load the bytecode image. If this fails, the
    runtime does _not_ fallback to any other mechanisms.
    
    On Windows, it is possible to recover the filename from a HANDLE. It is
    not portably possible to do this on Unix, so the filename which was
    opened by the stdlib/header.c is instead appended to the environment
    variable.

--- testsuite/tools/test_in_prefix.ml
+++ testsuite/tools/test_in_prefix.ml
@@ -2100,8 +2100,7 @@ let compile_test ~original env bindir libdir =
               {argv0_not_ocaml = false; argv0_resolved = test_program_relative}
             ] in
             let runs =
-              let test_with_outcome
-                    (({argv0; prefix_path_with_cwd; _} as test), properties) =
+              let test_with_outcome (({argv0; _} as test), properties) =
                 let {argv0_not_ocaml; argv0_resolved} = properties in
                 let outcome =
                   (* If runtime has been specified, this program is going to be
@@ -2117,34 +2116,19 @@ let compile_test ~original env bindir libdir =
                         Success {executable_name = test_program_path;
                                  argv0 = test_program_path}
                     | Tendered ->
-                        if argv0_not_ocaml then
-                          if no_caml_executable_name then
+                        if argv0_not_ocaml && no_caml_executable_name then
                             (* stdlib/header.c will fail to find ocamlrun
                                because caml_executable_name isn't implemented so
                                will either fail to find the executable or will
                                identify that it is not a bytecode executable.
                                Somewhat confusingly, it exits with code 2 *)
                             Fail 2
-                          else
-                            (* stdlib/header.c will find ocamlrun (because it
-                               effectively uses caml_executable_name) but fails
-                               to hand off the bytecode image, which causes
-                               ocamlrun to exit with code 127 *)
-                            Fail 127
-                        else if Sys.win32 then
-                          (* stdlib/header.c correctly preserves argv[0] *)
-                          Success {executable_name = test_program_path; argv0}
                         else
-                          (* stdlib/header.c correctly preserves argv[0] *)
                           let executable_name =
-                            (* XXX The Windows implementation uses SearchPath
-                                   which results explicit files in the cwd, but
-                                   that doesn't happen elsewhere *)
-                            if no_caml_executable_name
-                               || prefix_path_with_cwd then
+                            if no_caml_executable_name then
                               argv0_resolved
                             else
-                              argv0
+                              test_program_path
                           in
                           Success {executable_name; argv0}
                     | Custom ->
