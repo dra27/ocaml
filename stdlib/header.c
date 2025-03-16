65c5971770ded579f4a093f21236bd6229e990a2
David Allsopp <david.allsopp@metastack.com>
2025-03-16 15:27:29 +0000

    Share image fd between header.c and startup_byt.c
    The bytecode executable launcher (stdlib/header.c) can _only_ invoke
    ocamlrun after it has opened itself in order to find out the name of the
    runtime to execute. However, once ocamlrun is exec'd, the knowledge of
    this file was previously lost and if it could not be recovered from
    argv[0], then execution fails.
    
    This new approach, for both Windows and Unix, instead keeps the fd for
    the bytecode image open and passes its number to ocamlrun as
    __OCAML_EXEC_FD in the environment. ocamlrun detects this environment
    variable and uses that fd to load the bytecode image. If this fails, the
    runtime does _not_ fallback to any other mechanisms.
    
    On Windows, it is possible to recover the filename from a HANDLE. It is
    not portably possible to do this on Unix, so the filename which was
    opened by the stdlib/header.c is instead appended to the environment
    variable.

--- stdlib/header.c
+++ stdlib/header.c
@@ -55,6 +55,16 @@ typedef wchar_t * argv_t;
 #define CP CP_ACP
 #endif
 
+#ifndef __has_attribute
+#define __has_attribute(x) 0
+#endif
+
+#if __has_attribute(fallthrough)
+  #define fallthrough __attribute__ ((fallthrough))
+#else
+  #define fallthrough ((void) 0)
+#endif
+
 /* The maximum representable path for any API function, after internal expansion
    of \\?\ etc. is 32767 characters. PATH_MAX includes the terminator. */
 #define PATH_MAX 0x8000
@@ -66,6 +76,12 @@ static HANDLE hProcessHeap;
 #define free(memblock) HeapFree(hProcessHeap, 0, (memblock))
 
 #define SEEK_END FILE_END
+
+/* Initialised as the first statement of wmainCRTStartup */
+static HANDLE hProcessHeap;
+
+#define malloc(size) HeapAlloc(hProcessHeap, 0, (size))
+#define free(memblock) HeapFree(hProcessHeap, 0, (memblock))
 
 #define lseek(h, offset, origin) SetFilePointer((h), (offset), NULL, (origin))
 
@@ -88,10 +104,9 @@ static BOOL WINAPI ctrl_handler(DWORD event)
 
 #define safe_copy(s1, s2, n) lstrcpy(s1, s2)
 
-static int exec_file(wchar_t *file, wchar_t *cmdline)
+static int exec_file(wchar_t *file, wchar_t *cmdline, STARTUPINFO *stinfo)
 {
   LPWSTR truename = (LPWSTR)malloc(PATH_MAX * sizeof(WCHAR));
-  STARTUPINFO stinfo;
   PROCESS_INFORMATION procinfo;
   DWORD retcode;
 
@@ -103,9 +118,8 @@ static int exec_file(wchar_t *file, wchar_t *cmdline)
        underlying OCaml program with us! */
     SetConsoleCtrlHandler(ctrl_handler, TRUE);
 
-    GetStartupInfo(&stinfo);
     if (CreateProcess(truename, cmdline, NULL, NULL, TRUE, 0, NULL, NULL,
-                      &stinfo, &procinfo)) {
+                      stinfo, &procinfo)) {
       free(truename);
       CloseHandle(procinfo.hThread);
       WaitForSingleObject(procinfo.hProcess, INFINITE);
@@ -161,6 +175,7 @@ NORETURN static void exit_with_error(const wchar_t *wstr1,
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <limits.h>
@@ -214,7 +229,7 @@ NORETURN static void exit_with_error(const char *str1,
   exit(2);
 }
 
-static int exec_file(const char *file, char * const argv[])
+static int exec_file(const char *file, char * const argv[], void *_stinfo)
 {
   return (execvp(file, argv) == -1 ? errno : 0);
 }
@@ -271,7 +286,8 @@ static char * read_runtime_path(file_descriptor fd, uint32_t *path_size)
 }
 
 NORETURN void search_and_exec_runtime(char_os *rntm, uint32_t rntm_bsz,
-                                      argv_t argv, char_os *argv0_dirname)
+                                      argv_t argv, char_os *argv0_dirname,
+                                      void *stinfo)
 {
   char_os *rntm_end = rntm + (rntm_bsz - 1);
   char_os *rntm_bindir_end = rntm;
@@ -286,7 +302,7 @@ NORETURN void search_and_exec_runtime(char_os *rntm, uint32_t rntm_bsz,
     /* Absolute / Absolute_then_search (see bytecomp/bytelink.ml) */
     if (rntm_bindir_end != rntm_end)
       *rntm_bindir_end = Directory_separator_character;
-    int status = exec_file(rntm, argv);
+    int status = exec_file(rntm, argv, stinfo);
     if (rntm_bindir_end == rntm_end || status != ENOENT)
       exit_with_error(T("Cannot exec "), rntm, NULL);
   }
@@ -341,11 +357,11 @@ NORETURN void search_and_exec_runtime(char_os *rntm, uint32_t rntm_bsz,
            directory as the executable, it will be exec'd (even if that results
            in an error) */
         if (file_exists(root)) {
-          if (exec_file(root, argv) != 0)
+          if (exec_file(root, argv, stinfo) != 0)
             exit_with_error(T("Cannot exec "), root, NULL);
         }
       }
-      if (searched_all && exec_file(rntm, argv) != ENOENT)
+      if (searched_all && exec_file(rntm, argv, stinfo) != ENOENT)
         exit_with_error(T("Cannot exec "), rntm, NULL);
     } while (*current_quintet != 0);
   }
@@ -371,6 +387,11 @@ NORETURN void search_and_exec_runtime(char_os *rntm, uint32_t rntm_bsz,
 
 #ifdef _WIN32
 
+#undef RtlMoveMemory
+void __declspec(dllimport) __stdcall RtlMoveMemory(void *Destination,
+                                                   const void *Source,
+                                                   size_t Length);
+
 NORETURN void __cdecl wmainCRTStartup(void)
 {
   LPWSTR truename;
@@ -391,8 +412,13 @@ NORETURN void __cdecl wmainCRTStartup(void)
     exit_with_error(L"Out of memory", NULL, NULL);
   *basename = 0;
 
+  /* Mark the HANDLE as inheritable so ocamlrun can use it */
+  SECURITY_ATTRIBUTES sa;
+  sa.nLength = sizeof(sa);
+  sa.lpSecurityDescriptor = NULL;
+  sa.bInheritHandle = TRUE;
   h = CreateFile(truename, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE,
-                 NULL, OPEN_EXISTING, 0, NULL);
+                 &sa, OPEN_EXISTING, 0, NULL);
   if (h == INVALID_HANDLE_VALUE
       || (runtime_path = read_runtime_path(h, &rntm_strlen)) == NULL
       || (wruntime_path =
@@ -401,10 +427,111 @@ NORETURN void __cdecl wmainCRTStartup(void)
                                          wruntime_path, rntm_strlen + 1)) == 0)
     exit_with_error(NULL, truename,
                     L" not found or is not a bytecode executable file");
-  CloseHandle(h);
   free(runtime_path);
   free(truename);
-  search_and_exec_runtime(wruntime_path, rntm_bsz, GetCommandLine(), dirname);
+  STARTUPINFO stinfo;
+  /* Retrieve the existing STARTUPINFO structure - however this header was
+     invoked is morally how we should invoke ocamlrun, but we also need to
+     set-up or augment the cbReserved2 / lpReserved2 members in order to pass
+     the HANDLE h to ocamlrun as a CRT fd. The cloexec.ml test checks that
+     existing fds are passed through successfully. The use of lpReserved2 by the
+     CRT can be seen in the Universal CRT sources info exec/spawnv.cpp for the
+     code which sets the buffer up and in lowio/ioinit.cpp which reads the
+     buffer provided to the process. The semantics of this buffer are unchanged
+     since the very beginning of Windows NT.
+     It is a relatively well-documented "trick" to be able to pass up to 64KiB
+     of information to a new process using lpReserved2, on condition that the
+     data respects the CRTs requirements. The CRT processes lpReserved2 if it is
+     not NULL and if cbReserved2 is non-zero - it performs no further checking
+     beyond that. Applications can therefore embed additional data by setting
+     cbReserved2 to the actual size of lpReserved2 and simply ensuring that the
+     first 4 bytes pointed to by lpReserved2 are zero.
+     Cygwin uses this mechanism when invoking processes to allow the Cygwin DLL
+     to pick up the required information about the caller, amongst other things
+     to implement fork (it's also used as part of argument passing).
+     The code below must therefore cater for three cases:
+     1. cbReserved2 == 0 / lpReserved2 == NULL, in which case the structure must
+        be created
+     2. cbReserved2 > 0 but there are fewer than 3 fds in the structure, in
+        which case empty handles must be added so that our HANDLE is fd 3
+     3. cbReserved2 > 0 and there are already 3 or more fds in the structure, in
+        which case our HANDLE is appended to the end of the structure */
+  GetStartupInfo(&stinfo);
+
+  /* This header avoids the CRT to keep its size down - the Windows API doesn't
+     have anything sprintf-like, however, the largest fd-number fits comfortably
+     within a 16-bit wide character and we know that it will never be zero - the
+     number of the fd is therefore passed to ocamlrun as a single wide-character
+     string where the code-point represents the fd.
+     Nemo nunc te poteste servare. */
+  WCHAR fd[2] = {0, 0};
+
+  /* Match the CRTs check - ignore the existing values if either cbReserved2 is
+     zero _or_ lpReserved2 is NULL */
+  if (stinfo.cbReserved2 > 0 && stinfo.lpReserved2 == NULL)
+    stinfo.cbReserved2 = 0;
+
+  int existing_count = 0;
+  /* Work out the fd number for h */
+  if (stinfo.cbReserved2 > 0) {
+    existing_count = *(int *)stinfo.lpReserved2;
+    fd[0] = existing_count;
+    /* If there is a structure present, but it has no fds, discard it. */
+    if (existing_count == 0)
+      stinfo.cbReserved2 = 0;
+  }
+  /* Allow for the standard handles */
+  if (fd[0] < 3)
+    fd[0] = 3;
+
+  WORD buffer_size = sizeof(int) + (fd[0] + 1) * (1 + sizeof(HANDLE));
+  LPBYTE buffer = (LPBYTE)malloc(buffer_size);
+
+  /* Store the total number of handles */
+  *(int *)buffer = fd[0] + 1;
+
+  /* Copy the existing flags and HANDLEs */
+  if (stinfo.cbReserved2 > 0) {
+    RtlMoveMemory(buffer + sizeof(int), stinfo.lpReserved2 + sizeof(int),
+                  existing_count);
+    RtlMoveMemory(buffer + sizeof(int) + fd[0] + 1,
+                  stinfo.lpReserved2 + sizeof(int) + existing_count,
+                  existing_count * sizeof(HANDLE));
+  }
+
+  /* Pointers to the next slot for flags and the next slot for a HANDLE */
+  LPBYTE osflags =
+    buffer + sizeof(int) + existing_count;
+  LPHANDLE oshandles =
+    (LPHANDLE)(buffer + sizeof(int) + fd[0] + 1
+               + existing_count * sizeof(HANDLE));
+
+  /* Ensure the standard fds are populated. Unrolled to prevent cl requiring the
+     memset intrinsic. */
+  switch (existing_count) {
+    case 0:
+      *osflags++ = 0;
+      *oshandles++ = INVALID_HANDLE_VALUE;
+      fallthrough;
+    case 1:
+      *osflags++ = 0;
+      *oshandles++ = INVALID_HANDLE_VALUE;
+      fallthrough;
+    case 2:
+      *osflags++ = 0;
+      *oshandles++ = INVALID_HANDLE_VALUE;
+  }
+
+  /* Add h to the structure */
+  *osflags = 1;
+  *oshandles = h;
+
+  stinfo.cbReserved2 = buffer_size;
+  stinfo.lpReserved2 = buffer;
+
+  SetEnvironmentVariable(L"__OCAML_EXEC_FD", fd);
+  search_and_exec_runtime(wruntime_path, rntm_bsz,
+                          GetCommandLine(), dirname, &stinfo);
 }
 
 #else
@@ -429,7 +556,16 @@ int main(int argc, char *argv[])
   if (fd == -1 || (runtime_path = read_runtime_path(fd, &rntm_strlen)) == NULL)
     exit_with_error(NULL, truename,
                     " not found or is not a bytecode executable file");
-  close(fd);
+
+  size_t truename_len = strlen(truename);
+  char *value = (char *)malloc(10 + 1 + truename_len + 1);
+  snprintf(value, 11, "%u,", fd);
+  strcat(value, truename);
+#ifdef HAS_SETENV_UNSETENV
+  setenv("__OCAML_EXEC_FD", value, 1);
+#else
+#error "Require a way to set environment variables"
+#endif
 
 #ifdef HAS_LIBGEN_H
   argv0_dirname = dirname(strdup(truename));
@@ -437,7 +573,8 @@ int main(int argc, char *argv[])
   argv0_dirname = NULL;
 #endif
 
-  search_and_exec_runtime(runtime_path, rntm_strlen + 1, argv, argv0_dirname);
+  search_and_exec_runtime(runtime_path, rntm_strlen + 1, argv, argv0_dirname,
+                          NULL);
 }
 
 #endif /* defined(_WIN32) */
