48e2c162f8e3c2f09248113febbb65b6b9dec2b3
David Allsopp <david.allsopp@metastack.com>
2025-03-17 15:56:11 +0100

    Fix STARTUPINFO structure in stdlib/header.c
    If a CRT application (including, therefore, another OCaml program),
    exec's a bytecode program which uses the executable header, the
    cbReserved2 and lpReserved2 fields of the STARTUPINFO structure are
    quietly used by the CRT to pass handle information about open fds.
    
    The status checker in the cloexec.ml test has C stubs, which causes
    ocamltest to compile the bytecode version with -custom. The test is
    split to have an additional intermediate pure OCaml program which simply
    repeats either the Unix.execv or Unix.create_process call with the
    Sys.argv it was applied. In bytecode, ocamltest compiles this program
    just with -use-runtime, which means it uses the executable header on
    Windows.
    
    Without the change to header.c in this commit, that test begins to fail
    in bytecode on Windows, because the CRT information about inherited
    handles is not passed on to the status checker (the HANDLE values will
    have been inherited, as that's a kernel function, but the CRT structures
    regarding the fds are not initialised, which is what the checker than
    looks at).
    
    The fix here simplifies the code considerably - rather than initialising
    a fresh STARTUPINFO structure, we simply use GetStartupInfo to retrieve
    the one which was used to create the process itself and pass that to
    CreateProcess - cbReserved2 and lpReserved2 are therefore passed on, and
    the cloexec.ml test passes again.

--- stdlib/header.c
+++ stdlib/header.c
@@ -198,13 +198,17 @@ NORETURN void __cdecl wmainCRTStartup(void)
        the underlying OCaml program with us! */
     SetConsoleCtrlHandler(ctrl_handler, TRUE);
 
-    stinfo.cb = sizeof(stinfo);
-    stinfo.lpReserved = NULL;
-    stinfo.lpDesktop = NULL;
-    stinfo.lpTitle = NULL;
-    stinfo.dwFlags = 0;
-    stinfo.cbReserved2 = 0;
-    stinfo.lpReserved2 = NULL;
+    /* Rather than creating a fresh STARTUPINFO structure, simply pass on the
+       one which was used to launch the header. This is morally correct, but
+       also has a technical benefit - the documentation for STARTUPINFO states
+       that cbReserved2 and lpReserved2 must be 0 and NULL respectively, but
+       this is a lie! cbReserved2 is intended to describe the length of the
+       buffer pointed to be lpReserved2, and this facility was added to
+       Windows NT to allow the CRT to pass both the information about inherited
+       handles when one CRT application execs or spawns another. The cloexec.ml
+       test exercises this. By using GetStartupInfo, we simply pass through any
+       existing values. */
+    GetStartupInfo(&stinfo);
     if (CreateProcess(truename, GetCommandLine(), NULL, NULL, TRUE, 0,
                       NULL, NULL, &stinfo, &procinfo)) {
       CloseHandle(procinfo.hThread);
