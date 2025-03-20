d61d8ad536e60dda4a2d4d6ed532e02bad4625da
David Allsopp <david.allsopp@metastack.com>
2025-03-20 09:08:54 +0000

    Harden startup of -custom executables
    By default, ocamlrun first tries to resolve argv[0] to determine where
    the bytecode image is and then tries opening the executable image
    itself. This is obviously correct for ocamlrun, when being called using
    a shebang or executable header, but it's not correct for -custom
    executables where we _know_ that the bytecode image should be with the
    executable. To achieve this, a new mode is added to
    caml_byte_program_mode (and the existing ones renamed) such that
    caml_byte_program_mode is now STANDARD (for ocamlrun - the existing
    behaviour), APPENDED (for -custom executables - the new behaviour) and
    EMBEDDED (for -output-complete-exe/-output-obj - the original use of
    it).
    
    The mode is also set directly by the linker, rather than having a
    default in libcamlrun which is then overridden by the startup code for
    -output-complete-exe.
    
    In the new APPENDED mode, if caml_executable_name is implemented (i.e.
    it returns a string) then this file _must_ contain the bytecode image
    and no other mechanisms are used. On platforms where
    caml_executable_name is not implemented, APPENDED falls back to STANDARD
    for compatibility.
    
    Technically, this stops an argv[0] injection attack on setuid/setgid
    -custom bytecode executables, although setuid should be used with
    -output-complete-exe, if at all.

--- bytecomp/bytelink.ml
+++ bytecomp/bytelink.ml
@@ -728,7 +728,8 @@ let link_bytecode_as_c tolink outfile =
 \n#endif";
        output_without_guarded_primitives outchan "\
 \n#include <caml/mlvalues.h>\
-\n#include <caml/startup.h>\n";
+\n#include <caml/startup.h>\
+\nenum caml_byte_program_mode caml_byte_program_mode = EMBEDDED;\n";
        output_string outchan "\nstatic int caml_code[] = {\n";
        Symtable.init();
        clear_crc_interfaces ();
@@ -877,10 +878,13 @@ let link objfiles output_name =
          output_string poc "\
          #ifdef __cplusplus\n\
          extern \"C\" {\n\
-         #endif";
+         #endif\n\
+         #define CAML_INTERNALS";
          output_without_guarded_primitives poc
            "\n#include <caml/mlvalues.h>";
-         output_char poc '\n';
+         output_string poc "\n#include <caml/startup.h>\n\
+         \n\
+         enum caml_byte_program_mode caml_byte_program_mode = APPENDED;\n";
          Symtable.output_primitive_table poc;
          output_string poc "\
          #ifdef __cplusplus\n\
