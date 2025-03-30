80776abb67c4f41234667a613cf9a94301e1a252
David Allsopp <david.allsopp@metastack.com>
2025-03-30 10:48:46 +0100

    Test both bytecode header methods

--- utils/config.mli
+++ utils/config.mli
@@ -314,6 +314,9 @@ val ar_supports_response_files: bool
 val tsan : bool
 (** Whether ThreadSanitizer instrumentation is enabled *)
 
+val shebangscripts : bool
+(** Whether the target supports shebang scripts *)
+
 (** Access to configuration values *)
 val print_config : out_channel -> unit
 
