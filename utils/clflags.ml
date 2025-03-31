d6757b3fbab1fe845ea7e6e616350fd436c8cc97
David Allsopp <david.allsopp@metastack.com>
2025-03-31 22:03:33 +0100

    Tweak the type

--- utils/clflags.ml
+++ utils/clflags.ml
@@ -39,7 +39,6 @@ module Float_arg_helper = Arg_helper.Make (struct
 end)
 
 type launch_method = Executable | Shebang of string option
-type search_method = Absolute | Absolute_then_search | Search
 
 let objfiles = ref ([] : string list)         (* .cmo and .cma files *)
 and ccobjs = ref ([] : string list)           (* .o, .a, .so and -cclib -lxxx *)
@@ -91,7 +90,7 @@ and open_modules = ref []               (* -open *)
 and use_prims = ref ""                  (* -use-prims ... *)
 and use_runtime = ref ""                (* -use-runtime ... *)
 and launch_method = ref None            (* -launch-method ... *)
-and search_method = ref Absolute        (* -search-method ... *)
+and search_method = ref None            (* -search-method ... *)
 and plugin = ref false                  (* -plugin ... *)
 and principal = ref false               (* -principal *)
 and real_paths = ref true               (* -short-paths *)
