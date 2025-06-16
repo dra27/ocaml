de9a966c7a867d93448849230535b21d981167cf
David Allsopp <david.allsopp@metastack.com>
2025-06-16 11:18:18 +0100

    Post-bootstrap cleanup
    boot/ocamlc now supports everything that the main compiler supports.

--- utils/config.fixed.ml
+++ utils/config.fixed.ml
@@ -70,8 +70,4 @@ let target = host
 let systhread_supported = false
 let flexdll_dirs = []
 let suffixing = true
-(* Sys.win32 is runtime-based, so this is still technically a "fixed" value from
-   a bootstrap perspective. This expression can be removed post-bootstrap, as
-   the build system will explicitly use -launch-method for all bytecode
-   linking. *)
-let launch_method = if Sys.win32 then "exe" else "sh"
+let launch_method = "sh"
