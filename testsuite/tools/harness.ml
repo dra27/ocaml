fb4ce3472b0cec10d39cf2d36604af93313b7a2f
David Allsopp <david.allsopp@metastack.com>
2025-06-16 11:18:18 +0100

    Post-bootstrap cleanup
    boot/ocamlc now supports everything that the main compiler supports.

--- testsuite/tools/harness.ml
+++ testsuite/tools/harness.ml
@@ -457,8 +457,7 @@ module Import = struct
     bytecode_shebangs_by_default: bool;
     shebangscripts: bool;
     filename_mangling: bool;
-    libraries: string list list;
-    zinc_bootstrapped: bool
+    libraries: string list list
   }
 
   module Bytes = Bytes
