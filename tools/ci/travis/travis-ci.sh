8d921885ce6737197c22f6014afb65556d05c222
David Allsopp <david.allsopp@metastack.com>
2025-06-29 08:40:14 +0100

    Graft GitHub Actions CI as at PR#10043 on to 4.11

--- tools/ci/travis/travis-ci.sh
+++ tools/ci/travis/travis-ci.sh
@@ -14,6 +14,8 @@
 #*                                                                        *
 #**************************************************************************
 
+set -e
+
 # TRAVIS_COMMIT_RANGE has the form   <commit1>...<commit2>
 # TRAVIS_CUR_HEAD is <commit1>
 # TRAVIS_PR_HEAD is <commit2>
@@ -28,22 +30,29 @@
 #        |          /
 #  TRAVIS_MERGE_BASE
 #
-echo TRAVIS_COMMIT_RANGE=$TRAVIS_COMMIT_RANGE
-echo TRAVIS_COMMIT=$TRAVIS_COMMIT
-if [[ $TRAVIS_EVENT_TYPE = "pull_request" ]] ; then
+echo "TRAVIS_COMMIT_RANGE=$TRAVIS_COMMIT_RANGE"
+echo "TRAVIS_COMMIT=$TRAVIS_COMMIT"
+if [[ $TRAVIS_EVENT_TYPE = 'pull_request' ]] ; then
   FETCH_HEAD=$(git rev-parse FETCH_HEAD)
-  echo FETCH_HEAD=$FETCH_HEAD
+  echo "FETCH_HEAD=$FETCH_HEAD"
 else
   FETCH_HEAD=$TRAVIS_COMMIT
 fi
 
-if [[ $TRAVIS_COMMIT != $(git rev-parse FETCH_HEAD) ]] ; then
-  echo "WARNING! Travis TRAVIS_COMMIT and FETCH_HEAD do not agree!"
-  if git cat-file -e $TRAVIS_COMMIT 2> /dev/null ; then
-    echo "TRAVIS_COMMIT exists, so going with it"
-  else
-    echo "TRAVIS_COMMIT does not exist; setting to FETCH_HEAD"
-    TRAVIS_COMMIT=$FETCH_HEAD
+if [[ $TRAVIS_EVENT_TYPE = 'push' ]] ; then
+  if ! git cat-file -e "$TRAVIS_COMMIT" 2> /dev/null ; then
+    echo 'TRAVIS_COMMIT does not exist - CI failure'
+    exit 1
+  fi
+else
+  if [[ $TRAVIS_COMMIT != $(git rev-parse FETCH_HEAD) ]] ; then
+    echo 'WARNING! Travis TRAVIS_COMMIT and FETCH_HEAD do not agree!'
+    if git cat-file -e "$TRAVIS_COMMIT" 2> /dev/null ; then
+      echo 'TRAVIS_COMMIT exists, so going with it'
+    else
+      echo 'TRAVIS_COMMIT does not exist; setting to FETCH_HEAD'
+      TRAVIS_COMMIT=$FETCH_HEAD
+    fi
   fi
 fi
 
@@ -51,7 +60,8 @@ set -x
 
 PREFIX=~/local
 
-MAKE=make SHELL=dash
+MAKE="make $MAKE_ARG"
+SHELL=dash
 
 TRAVIS_CUR_HEAD=${TRAVIS_COMMIT_RANGE%%...*}
 TRAVIS_PR_HEAD=${TRAVIS_COMMIT_RANGE##*...}
@@ -59,14 +69,68 @@ case $TRAVIS_EVENT_TYPE in
    # If this is not a pull request then TRAVIS_COMMIT_RANGE may be empty.
    pull_request)
      DEEPEN=50
-     while ! git merge-base $TRAVIS_CUR_HEAD $TRAVIS_PR_HEAD > /dev/null 2>&1
+     while ! git merge-base "$TRAVIS_CUR_HEAD" "$TRAVIS_PR_HEAD" >& /dev/null
      do
-       echo Deepening $TRAVIS_BRANCH by $DEEPEN commits
-       git fetch origin --deepen=$DEEPEN $TRAVIS_BRANCH
+       echo "Deepening $TRAVIS_BRANCH by $DEEPEN commits"
+       git fetch origin --deepen=$DEEPEN "$TRAVIS_BRANCH"
        ((DEEPEN*=2))
      done
-     TRAVIS_MERGE_BASE=$(git merge-base $TRAVIS_CUR_HEAD $TRAVIS_PR_HEAD);;
+     TRAVIS_MERGE_BASE=$(git merge-base "$TRAVIS_CUR_HEAD" "$TRAVIS_PR_HEAD");;
 esac
+
+CheckSyncStdlibDocs () {
+  cat<<EOF
+------------------------------------------------------------------------
+This test checks that running tools/sync-stdlib-docs is a no-op in the current
+state, which means that the labelled/unlabelled .mli files are in sync.  If
+this check fails, it should be fixable by just running the script and reviewing
+the changes it makes.
+------------------------------------------------------------------------
+EOF
+  tools/sync_stdlib_docs
+  git diff --quiet --exit-code && result=pass || result=fail
+  case $result in
+      pass)
+          echo "CheckSyncStdlibDocs: success";;
+      fail)
+          echo "CheckSyncStdlibDocs: failure with the following differences:"
+          git --no-pager diff
+          exit 1;;
+  esac
+}
+
+CheckDepend () {
+  cat<<EOF
+------------------------------------------------------------------------
+This test checks that 'alldepend' target is a no-op in the current
+state, which means that dependencies are correctly stored in .depend
+files. It should only be run after the compiler has been built.
+If this check fails, it should be fixable by just running 'make alldepend'.
+------------------------------------------------------------------------
+EOF
+  ./configure --disable-dependency-generation \
+              --disable-debug-runtime \
+              --disable-instrumented-runtime
+  # Need a runtime
+  $MAKE -j coldstart
+  # And generated files (ocamllex compiles ocamlyacc)
+  $MAKE -j ocamllex
+  $MAKE alldepend
+  # note: we cannot use $? as (set -e) may be set globally,
+  # and disabling it locally is not worth the hassle.
+  # note: we ignore the whitespace in case different C dependency
+  # detectors use different indentation styles.
+  git diff --ignore-all-space --quiet --exit-code **.depend \
+      && result=pass || result=fail
+  case $result in
+      pass)
+          echo "CheckDepend: success";;
+      fail)
+          echo "CheckDepend: failure with the following differences:"
+          git --no-pager diff --ignore-all-space **.depend
+          exit 1;;
+  esac
+}
 
 BuildAndTest () {
   mkdir -p $PREFIX
@@ -81,17 +145,40 @@ request can be merged.
 ------------------------------------------------------------------------
 EOF
 
-  configure_flags="\
-    --prefix=$PREFIX \
-    --enable-flambda-invariants \
-    $CONFIG_ARG"
+  # Ensure that make distclean can be run from an empty tree
+  $MAKE distclean
+
+  if [ "$MIN_BUILD" = "1" ] ; then
+    configure_flags="\
+      --prefix=$PREFIX \
+      --disable-shared \
+      --disable-debug-runtime \
+      --disable-instrumented-runtime \
+      --disable-systhreads \
+      --disable-str-lib \
+      --disable-unix-lib \
+      --disable-bigarray-lib \
+      --disable-ocamldoc \
+      --disable-native-compiler \
+      --enable-ocamltest \
+      --disable-dependency-generation \
+      $CONFIG_ARG"
+  else
+    configure_flags="\
+      --prefix=$PREFIX \
+      --enable-flambda-invariants \
+      --enable-ocamltest \
+      --disable-dependency-generation \
+      $CONFIG_ARG"
+  fi
   case $XARCH in
   x64)
     ./configure $configure_flags
     ;;
   i386)
-    ./configure --build=x86_64-pc-linux-gnu --host=i386-pc-linux-gnu \
-      AS="as" ASPP="gcc -c" \
+    ./configure --build=x86_64-pc-linux-gnu --host=i386-linux \
+      CC='gcc -m32' AS='as --32' ASPP='gcc -m32 -c' \
+      PARTIALLD='ld -r -melf_i386' \
       $configure_flags
     ;;
   *)
@@ -101,23 +188,59 @@ EOF
   esac
 
   export PATH=$PREFIX/bin:$PATH
-  $MAKE world.opt
-  $MAKE ocamlnat
+  if [ "$MIN_BUILD" = "1" ] ; then
+    if $MAKE world.opt ; then
+      echo "world.opt is not supposed to work!"
+      exit 1
+    else
+      $MAKE world
+    fi
+  else
+    $MAKE world.opt
+    $MAKE ocamlnat
+  fi
+  echo Ensuring that all names are prefixed in the runtime
+  ./tools/check-symbol-names runtime/*.a
   cd testsuite
   echo Running the testsuite with the normal runtime
   $MAKE all
-  echo Running the testsuite with the debug runtime
-  $MAKE USE_RUNTIME="d" OCAMLTESTDIR=$(pwd)/_ocamltestd TESTLOG=_logd all
+  if [ "$MIN_BUILD" != "1" ] ; then
+    echo Running the testsuite with the debug runtime
+    $MAKE USE_RUNTIME='d' OCAMLTESTDIR="$(pwd)/_ocamltestd" TESTLOG=_logd all
+  fi
   cd ..
+  if command -v pdflatex &>/dev/null  ; then
+    echo Ensuring that all library documentation compiles
+    $MAKE -C ocamldoc html_doc pdf_doc texi_doc
+  fi
   $MAKE install
-  echo Check the code examples in the manual
-  $MAKE manual-pregen
+  if command -v hevea &>/dev/null ; then
+    echo Ensuring that the manual compiles
+    # These steps rely on the compiler being installed and in PATH
+    $MAKE -C manual/manual/html_processing duniverse
+    $MAKE -C manual web
+  fi
+  if fgrep 'SUPPORTS_SHARED_LIBRARIES=true' Makefile.config &>/dev/null ; then
+    echo Check the code examples in the manual
+    $MAKE manual-pregen
+  fi
   # check_all_arches checks tries to compile all backends in place,
   # we would need to redo (small parts of) world.opt afterwards to
   # use the compiler again
   $MAKE check_all_arches
+  # Ensure that .gitignore is up-to-date - this will fail if any untreacked or
+  # altered files exist.
+  test -z "$(git status --porcelain)"
   # check that the 'clean' target also works
   $MAKE clean
+  $MAKE -C manual clean
+  # check that the `distclean` target definitely cleans the tree
+  $MAKE distclean
+  $MAKE -C manual distclean
+  # Check the working tree is clean
+  test -z "$(git status --porcelain)"
+  # Check that there are no ignored files
+  test -z "$(git ls-files --others -i --exclude-standard)"
 }
 
 CheckChangesModified () {
@@ -137,16 +260,16 @@ on the github pull request.
 ------------------------------------------------------------------------
 EOF
   # check that Changes has been modified
-  git diff $TRAVIS_MERGE_BASE..$TRAVIS_PR_HEAD --name-only --exit-code Changes \
-    > /dev/null && CheckNoChangesMessage || echo pass
+  git diff "$TRAVIS_MERGE_BASE..$TRAVIS_PR_HEAD" --name-only --exit-code \
+    Changes > /dev/null && CheckNoChangesMessage || echo pass
 }
 
 CheckNoChangesMessage () {
   API_URL=https://api.github.com/repos/$TRAVIS_REPO_SLUG/issues/$TRAVIS_PULL_REQUEST/labels
-  if test -n "$(git log --grep="[Nn]o [Cc]hange.* needed" --max-count=1 \
-    ${TRAVIS_MERGE_BASE}..${TRAVIS_PR_HEAD})"
+  if [[ -n $(git log --grep='[Nn]o [Cc]hange.* needed' --max-count=1 \
+    "$TRAVIS_MERGE_BASE..$TRAVIS_PR_HEAD") ]]
   then echo pass
-  elif test -n "$(curl $API_URL | grep 'no-change-entry-needed')"
+  elif [[ -n $(curl "$API_URL" | grep 'no-change-entry-needed') ]]
   then echo pass
   else exit 1
   fi
@@ -155,13 +278,14 @@ CheckNoChangesMessage () {
 CheckManual () {
       cat<<EOF
 --------------------------------------------------------------------------
-This test checks that all standard library modules are referenced by the
-standard library chapter of the manual.
+This test checks the global structure of the reference manual
+(e.g. missing chapters).
 --------------------------------------------------------------------------
 EOF
   # we need some of the configuration data provided by configure
   ./configure
-  $MAKE check-stdlib -C manual/tests
+  $MAKE check-stdlib check-case-collision -C manual/tests
+
 }
 
 CheckTestsuiteModified () {
@@ -184,14 +308,14 @@ does *not* imply that your change is appropriately tested.
 ------------------------------------------------------------------------
 EOF
   # check that at least a file in testsuite/ has been modified
-  git diff $TRAVIS_MERGE_BASE..$TRAVIS_PR_HEAD --name-only --exit-code \
+  git diff "$TRAVIS_MERGE_BASE..$TRAVIS_PR_HEAD" --name-only --exit-code \
     testsuite > /dev/null && exit 1 || echo pass
 }
 
 # Test to see if any part of the directory name has been marked prune
 not_pruned () {
   DIR=$(dirname "$1")
-  if [ "$DIR" = "." ] ; then
+  if [[ $DIR = '.' ]] ; then
     return 0
   else
     case ",$(git check-attr typo.prune "$DIR" | sed -e 's/.*: //')," in
@@ -200,7 +324,7 @@ not_pruned () {
       ;;
       *)
 
-      not_pruned $DIR
+      not_pruned "$DIR"
       return $?
     esac
   fi
@@ -209,15 +333,15 @@ not_pruned () {
 CheckTypoTree () {
   export OCAML_CT_HEAD=$1
   export OCAML_CT_LS_FILES="git diff-tree --no-commit-id --name-only -r $2 --"
-  export OCAML_CT_CAT="git cat-file --textconv"
+  export OCAML_CT_CAT='git cat-file --textconv'
   export OCAML_CT_PREFIX="$1:"
-  GIT_INDEX_FILE=tmp-index git read-tree --reset -i $1
-  git diff-tree --diff-filter=d --no-commit-id --name-only -r $2 \
+  GIT_INDEX_FILE=tmp-index git read-tree --reset -i "$1"
+  git diff-tree --diff-filter=d --no-commit-id --name-only -r "$2" \
     | (while IFS= read -r path
   do
-    if not_pruned $path ; then
+    if not_pruned "$path" ; then
       echo "Checking $1: $path"
-      if ! tools/check-typo ./$path ; then
+      if ! tools/check-typo "./$path" ; then
         touch check-typo-failed
       fi
     else
@@ -229,10 +353,10 @@ CheckTypoTree () {
     esac
   done)
   rm -f tmp-index
-  if [ -e CHECK_CONFIGURE ] ; then
+  if [[ -e CHECK_CONFIGURE ]] ; then
     rm -f CHECK_CONFIGURE
     echo "configure generation altered in $1"
-    echo "Verifying that configure.ac generates configure"
+    echo 'Verifying that configure.ac generates configure'
     git checkout "$1"
     mv configure configure.ref
     make configure
@@ -247,33 +371,32 @@ please run rm configure ; make configure and commit"
 CHECK_ALL_COMMITS=0
 
 CheckTypo () {
-  export OCAML_CT_GIT_INDEX="tmp-index"
-  export OCAML_CT_CA_FLAG="--cached"
+  export OCAML_CT_GIT_INDEX='tmp-index'
+  export OCAML_CT_CA_FLAG='--cached'
   # Work around an apparent bug in Ubuntu 12.4.5
   # See https://bugs.launchpad.net/ubuntu/+source/gawk/+bug/1647879
-  export OCAML_CT_AWK="awk --re-interval"
   rm -f check-typo-failed
-  if test -z "$TRAVIS_COMMIT_RANGE"
-  then CheckTypoTree $TRAVIS_COMMIT $TRAVIS_COMMIT
+  if [[ -z $TRAVIS_COMMIT_RANGE ]]
+  then CheckTypoTree "$TRAVIS_COMMIT" "$TRAVIS_COMMIT"
   else
-    if [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]
+    if [[ $TRAVIS_EVENT_TYPE = 'pull_request' ]]
     then TRAVIS_COMMIT_RANGE=$TRAVIS_MERGE_BASE..$TRAVIS_PULL_REQUEST_SHA
     fi
-    if [ $CHECK_ALL_COMMITS -eq 1 ]
+    if [[ $CHECK_ALL_COMMITS -eq 1 ]]
     then
-      for commit in $(git rev-list $TRAVIS_COMMIT_RANGE --reverse)
+      for commit in $(git rev-list "$TRAVIS_COMMIT_RANGE" --reverse)
       do
-        CheckTypoTree $commit $commit
+        CheckTypoTree "$commit" "$commit"
       done
     else
-      if [ -z "$TRAVIS_PULL_REQUEST_SHA" ]
-      then CheckTypoTree $TRAVIS_COMMIT $TRAVIS_COMMIT
-      else CheckTypoTree $TRAVIS_COMMIT $TRAVIS_COMMIT_RANGE
+      if [[ -z $TRAVIS_PULL_REQUEST_SHA ]]
+      then CheckTypoTree "$TRAVIS_COMMIT" "$TRAVIS_COMMIT"
+      else CheckTypoTree "$TRAVIS_COMMIT" "$TRAVIS_COMMIT_RANGE"
       fi
     fi
   fi
   echo complete
-  if [ -e check-typo-failed ]
+  if [[ -e check-typo-failed ]]
   then exit 1
   fi
 }
@@ -294,6 +417,9 @@ tests)
 check-typo)
    set +x
    CheckTypo;;
+check-depend)
+    CheckSyncStdlibDocs
+    CheckDepend;;
 *) echo unknown CI kind
    exit 1
    ;;
