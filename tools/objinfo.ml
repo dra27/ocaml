4856a1ef3d930e8ba4fb5aaead3cea19ed5e9115
David Allsopp <david.allsopp@metastack.com>
2025-07-15 16:39:57 +0100

    Basis

--- tools/objinfo.ml
+++ tools/objinfo.ml
@@ -31,6 +31,8 @@ let no_crc = ref false
 
 module Magic_number = Misc.Magic_number
 
+let yesno_of_bool oc b = output_string oc (if b then "YES" else "no")
+
 let input_stringlist ic len =
   let get_string_list sect len =
     let rec fold s e acc =
@@ -76,13 +78,13 @@ let print_cmo_infos cu =
         printf "YES\n";
         printf "Primitives declared in this module:\n";
         List.iter print_line l);
-  printf "Force link: %s\n" (if cu.cu_force_link then "YES" else "no")
+  printf "Force link: %a\n" yesno_of_bool cu.cu_force_link
 
 let print_spaced_string s =
   printf " %s" s
 
 let print_cma_infos (lib : Cmo_format.library) =
-  printf "Force custom: %s\n" (if lib.lib_custom then "YES" else "no");
+  printf "Force custom: %a\n" yesno_of_bool lib.lib_custom;
   printf "Extra C object files:";
   (* PR#4949: print in linking order *)
   List.iter print_spaced_string (List.rev lib.lib_ccobjs);
@@ -173,7 +175,9 @@ let print_cmx_infos (ui, crc) =
   printf "Currying functions:%a\n" pr_funs ui.ui_curry_fun;
   printf "Apply functions:%a\n" pr_funs ui.ui_apply_fun;
   printf "Send functions:%a\n" pr_funs ui.ui_send_fun;
-  printf "Force link: %s\n" (if ui.ui_force_link then "YES" else "no")
+  printf "Force link: %a\n" yesno_of_bool ui.ui_force_link;
+  printf
+    "Requires caml_standard_library_nat: %a\n" yesno_of_bool ui.ui_need_stdlib
 
 let print_cmxa_infos (lib : Cmx_format.library_infos) =
   printf "Extra C object files:";
@@ -208,10 +212,62 @@ let p_list title print = function
       p_title title;
       List.iter print l
 
+let p_runtime_id ({Misc.RuntimeID.dev; release; no_flat_float_array; fp; tsan;
+                   int31; static; naked_pointers; mutable_string; ansi;
+                   reserved} as t) =
+  let version =
+    if release > Config.release_number then
+      ""
+    else
+      if release = 0 then
+        " (Objective Caml 3.12)"
+      else if release < 16 then
+        Printf.sprintf " (OCaml 4.%02d)" (release - 1)
+      else
+        Printf.sprintf " (OCaml 5.%d)" (release - 16)
+  in
+  printf "\t%s = Release %d%s%s\n"
+    (Misc.RuntimeID.to_string t)
+    release version (if dev then " - development/altered version" else "");
+  if reserved > 0 then
+    printf "\t  - %d reserved header bit%s\n"
+      reserved (if reserved = 1 then "" else "s");
+  if no_flat_float_array then
+    printf "\t  - Flat float array representation disabled\n";
+  if fp then
+    printf "\t  - Frame pointers enabled\n";
+  if tsan then
+    printf "\t  - TSAN enabled\n";
+  if int31 then
+    printf "\t  - Compiled without 64-bit support\n";
+  if static then
+    printf "\t  - Compiled without support dynamic loading\n";
+  if not naked_pointers then
+    printf "\t  - Compiled without support for naked pointers\n";
+  if not mutable_string then
+    printf "\t  - Compiled with immutable strings\n";
+  if ansi then
+    printf "\t  - Windows Unicode support disabled\n"
+
+let p_runtime (runtime, id, search) =
+  let runtime =
+    let some id = runtime ^ "-" ^ Misc.RuntimeID.to_string id in
+    Option.fold ~none:runtime ~some id
+  in
+  let runtime =
+    match search with
+    | Byterntm.Search -> runtime
+    | Byterntm.Absolute dir -> dir ^ runtime
+    | Byterntm.Absolute_then_search dir -> Printf.sprintf "[%s]%s" dir runtime
+  in
+  printf "Runtime:\n\t%s\n" runtime;
+  Option.iter p_runtime_id id
+
 let dump_byte ic =
   Bytesections.read_toc ic;
   let toc = Bytesections.toc () in
   let toc = List.sort Stdlib.compare toc in
+  Option.iter p_runtime (Byterntm.read_runtime ic);
   List.iter
     (fun (section, _) ->
        try
@@ -238,6 +294,11 @@ let dump_byte ic =
                  (input_stringlist ic len)
            | "SYMB" ->
                print_global_table (input_value ic)
+           | "OSLD" ->
+               let caml_standard_library_default =
+                 Bytesections.read_section_string ic section in
+               printf "caml_standard_library_default: %s\n"
+                      caml_standard_library_default
            | _ -> ()
        with _ -> ()
     )
