efe9f8c6e2fa52af2d62e526be16e8a988a80325
Florian Angeletti <florian.angeletti@inria.fr>
2025-11-28 17:40:42 +0100

    Merge pull request PR#14243 from dra27/ld.conf
    Relocatable OCaml - explicit-relative paths in `ld.conf`
    
    (cherry picked from commit 1e05f341eaf6a7e953332e6081c86b275959191d)

--- runtime/caml/osdeps.h
+++ runtime/caml/osdeps.h
@@ -138,6 +138,14 @@ CAMLextern value caml_win32_xdg_defaults(void);
 
 CAMLextern value caml_win32_get_temp_path(void);
 
+#define CAML_DIR_SEP T("\\")
+#define Is_separator(c) (c == '\\' || c == '/')
+
+#else
+
+#define CAML_DIR_SEP T("/")
+#define Is_separator(c) (c == '/')
+
 #endif /* _WIN32 */
 
 /* Returns the current value of a counter that increments once per nanosecond.
