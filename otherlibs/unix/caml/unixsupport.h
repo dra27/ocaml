9d28ec5f6c3ca2cce58e47e447e7beb3aed46922
David Allsopp <david.allsopp@metastack.com>
2026-01-22 17:23:59 +0000

    Revert "Merge pull request PR#14429 from MisterDA/cxx-unixsupport-stdatomic"
    This reverts commit 0e8a8eb68408236f0bc6467b383fbf78a66c65df, reversing
    changes made to 5b902eb98d980a9489d872ffe179b53eb9b05439.

--- otherlibs/unix/caml/unixsupport.h
+++ otherlibs/unix/caml/unixsupport.h
@@ -44,9 +44,6 @@ struct filedescr {
     SOCKET socket;
   } fd;                   /* Real windows handle */
   enum { KIND_HANDLE, KIND_SOCKET } kind;
-#ifdef __cplusplus
-  std::
-#endif
   atomic_int crt_fd;      /* C runtime descriptor */
   unsigned int flags_fd;  /* See FLAGS_FD_* */
 };
